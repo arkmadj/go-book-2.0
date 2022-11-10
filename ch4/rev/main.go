ba880cacd3ed9201ff53bd43d583ed7d6e040bbd
arkmadj <ahmadjinadu@gmail.com>
2022-11-10 07:09:52 +0100

    Added equal to comparision for slices

--- ch4/rev/main.go
+++ ch4/rev/main.go
@@ -10,10 +10,29 @@ func reverse(s []int) {
 	}
 }
 
+func equal(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
 func main() {
 	a := []int{0, 1, 2, 3, 4, 5}
+	b := []string{"0", "1", "2", "3", "4", "5"}
+	c := []string{"0", "1", "2", "6", "4", "5"}
+	d := []string{"0", "1", "2", "7", "4", "5"}
 	reverse(a[:2])
 	reverse(a[2:])
 	reverse(a)
 	fmt.Println(a)
+	fmt.Println(equal(b, c))
+	fmt.Println(equal(b, d))
+	fmt.Println(equal(c, d))
 }
