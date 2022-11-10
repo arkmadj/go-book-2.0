af09acef2c5ef4cd20c64b0e7faf056d497a70d4
arkmadj <ahmadjinadu@gmail.com>
2022-11-10 08:19:25 +0100

    Used append for add to non empty

--- ch4/nonempty/main.go
+++ ch4/nonempty/main.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 func nonempty(strings []string) []string {
-	i := 0
+	out := strings[:0]
 	for _, s := range strings {
 		if s != "" {
-			strings[i] = s
-			i++
+			out = append(out, s)
 		}
 	}
-	return strings[:i]
+	return out
 }
 
 func main() {
