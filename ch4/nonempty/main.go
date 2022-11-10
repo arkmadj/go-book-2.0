782db4f53bdc38c5b1d173295ff1917371a55854
arkmadj <ahmadjinadu@gmail.com>
2022-11-10 08:18:06 +0100

    Added add nonempty

---
+++ ch4/nonempty/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func nonempty(strings []string) []string {
+	i := 0
+	for _, s := range strings {
+		if s != "" {
+			strings[i] = s
+			i++
+		}
+	}
+	return strings[:i]
+}
+
+func main() {
+	data := []string{"one", "", "three"}
+	fmt.Printf("%q\n", nonempty(data))
+	fmt.Printf("%q\n", data)
+}
