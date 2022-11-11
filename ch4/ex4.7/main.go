d9175bf73f80832c7ab1a35888b88a931c4476f2
arkmadj <ahmadjinadu@gmail.com>
2022-11-11 07:56:52 +0100

    Added revUT8

--- ch4/ex4.7/main.go
+++ ch4/ex4.7/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func reverse(b []byte) {
 	size := len(b)
@@ -9,8 +12,21 @@ func reverse(b []byte) {
 	}
 }
 
+func revUTF8(b []byte) []byte {
+	for i := 0; i < len(b); {
+		_, size := utf8.DecodeLastRune((b[1:]))
+		reverse(b[i : i+size])
+		i += size
+	}
+	reverse(b)
+	return b
+}
+
 func main() {
 	a := []byte("Ahmad Jinadu")
+	b := []byte("Monkey D. Luffy")
 	reverse(a)
+	revUTF8(b)
 	fmt.Println(string(a))
+	fmt.Println(string(b))
 }
