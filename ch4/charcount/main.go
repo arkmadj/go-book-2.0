ad3d8ea317db3c8de12f1a5c0c7953f339363041
arkmadj <ahmadjinadu@gmail.com>
2022-11-13 00:08:32 +0100

    Competed charcount

--- ch4/charcount/main.go
+++ ch4/charcount/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"io"
 	"os"
+	"unicode"
 	"unicode/utf8"
 )
 
-func main(){
+func main() {
 	counts := make(map[rune]int)
 	var utflen [utf8.UTFMax + 1]int
 	invalid := 0
@@ -14,6 +17,31 @@ func main(){
 	in := bufio.NewReader(os.Stdin)
 	for {
 		r, n, err := in.ReadRune()
-		if err != niul
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+	fmt.Printf("Rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
 	}
 }
