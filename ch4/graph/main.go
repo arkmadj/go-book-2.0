f0047ba61317957d3b247ac85d6216491c1fa65c
arkmadj <ahmadjinadu@gmail.com>
2022-11-13 11:41:23 +0100

    Tested graph

--- ch4/graph/main.go
+++ ch4/graph/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 var graph = make(map[string]map[string]bool)
 
 func addEdge(from, to string) {
@@ -19,5 +21,11 @@ func main() {
 	a := [...]string{"a", "b", "c", "d", "e", "f"}
 	for i := 0; i < len(a)-1; i++ {
 		addEdge(a[i], a[i+1])
+	}
+	for i := 0; i < len(a)-1; i++ {
+		fmt.Println(hasEdge(a[i], a[i+1]))
+	}
+	for i := 0; i < len(a)-2; i++ {
+		fmt.Println(hasEdge(a[i], a[i+2]))
 	}
 }
