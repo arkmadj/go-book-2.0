e3244caddbd758281393d1626aa501faa91784fd
arkmadj <ahmadjinadu@gmail.com>
2022-11-13 11:38:09 +0100

    Added graph

---
+++ ch4/graph/main.go
@@ -0,0 +1,23 @@
+package main
+
+var graph = make(map[string]map[string]bool)
+
+func addEdge(from, to string) {
+	edges := graph[from]
+	if edges == nil {
+		edges = make(map[string]bool)
+		graph[from] = edges
+	}
+	edges[to] = true
+}
+
+func hasEdge(from, to string) bool {
+	return graph[from][to]
+}
+
+func main() {
+	a := [...]string{"a", "b", "c", "d", "e", "f"}
+	for i := 0; i < len(a)-1; i++ {
+		addEdge(a[i], a[i+1])
+	}
+}
