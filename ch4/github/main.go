f44c0429f3ccef45a1fc8107a3862eea8820fb24
arkmadj <ahmadjinadu@gmail.com>
2022-11-14 23:33:35 +0100

    Added seearch function

--- ch4/github/main.go
+++ ch4/github/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
 
 const IssuesURL = "https://api.github.com/search/issues"
 
@@ -22,4 +29,25 @@ type Issue struct {
 type User struct {
 	Login   string
 	HTMLURL string `json:"html_url"`
+}
+
+func SearchIssues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, ""))
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("Search query failed: %s", resp.Status)
+	}
+
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
 }
