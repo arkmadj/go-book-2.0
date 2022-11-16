339d05fdcd1d5c78de895a68e58a18d1db73778e
arkmadj <ahmadjinadu@gmail.com>
2022-11-16 07:19:01 +0100

    Added get issues

---
+++ ch4/ex4.10/search.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func SearchISuues(terms []string) (*IssuesSearchResult, error) {
+	q := url.QueryEscape(strings.Join(terms, ""))
+	resp, err := http.Get(IssuesURL + "?q=" + q)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("Search query failed: %s", resp.Status)
+	}
+
+	var result IssuesSearchResult
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
+
+func get(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("Can't get %s: %s", url, resp.Status)
+	}
+	return resp, nil
+}
+
+func GetIssue(owner string, repo string, number string) (*Issue, error) {
+	url := strings.Join([]string{APIURL, "repos", owner, repo, "issues", number}, "/")
+	resp, err := get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var issue Issue
+	if err := json.NewDecoder(resp.Body).Decode(&issue); err != nil{
+		return nil, err
+	}
+	return &issue, nil
+}
+
+func GetIssues(owner string, re)
