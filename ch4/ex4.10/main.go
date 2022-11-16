ced34e92b0ba0f6f17eee0e407576ebc559560b0
arkmadj <ahmadjinadu@gmail.com>
2022-11-16 07:40:30 +0100

    Added read

--- ch4/ex4.10/main.go
+++ ch4/ex4.10/main.go
@@ -15,3 +15,15 @@ func search(query []string) {
 		fmt.Printf(format, item.Number, item.User.Login, item.Title)
 	}
 }
+
+func read(owner, repo, number string) {
+	issue, err := GetIssue(owner, repo, number)
+	if err != nil {
+		log.Fatal(err)
+	}
+	body := issue.Body
+	if body == "" {
+		body = "<empty>\n"
+	}
+	fmt.Printf("Repo: %s%s\nNumber: %s\nUser: %s\nTitle: %s\n\n%s", owner, repo, number, issue.User.Login, issue.Title, body)
+}
