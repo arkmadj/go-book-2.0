415b436b5d8cf17bd7d72e60912f3d61d6b9532e
arkmadj <ahmadjinadu@gmail.com>
2022-11-16 08:06:33 +0100

    Added main.go

--- ch4/ex4.10/github.go
+++ ch4/ex4.10/github.go
@@ -19,7 +19,7 @@ type Issue struct {
 	Title     string
 	State     string
 	User      *User
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"createdat"`
 	Body      string
 }
 
