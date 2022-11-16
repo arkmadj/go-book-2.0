f41abe4f3220435b30712d3045b8c4800fa9c81d
arkmadj <ahmadjinadu@gmail.com>
2022-11-16 07:53:54 +0100

    Added edit

--- ch4/ex4.10/main.go
+++ ch4/ex4.10/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"os/exec"
 )
 
 func search(query []string) {
@@ -26,4 +30,59 @@ func read(owner, repo, number string) {
 		body = "<empty>\n"
 	}
 	fmt.Printf("Repo: %s%s\nNumber: %s\nUser: %s\nTitle: %s\n\n%s", owner, repo, number, issue.User.Login, issue.Title, body)
+}
+
+func edit(owner, repo, number string) {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		editor = "vim"
+	}
+	editorPath, err := exec.LookPath(editor)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tempfile, err := ioutil.TempFile("", "issue_card")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tempfile.Close()
+	defer os.Remove(tempfile.Name())
+
+	issue, err := GetIssue(owner, repo, number)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	encoder := json.NewEncoder(tempfile)
+	err = encoder.Encode(map[string]string{
+		"title": issue.Title,
+		"state": issue.State,
+		"body":  issue.Body,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := &exec.Cmd{
+		Path:   editorPath,
+		Args:   []string{editor, tempfile.Name()},
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tempfile.Seek(0, 0)
+	fields := make(map[string]string)
+	if err = json.NewDecoder(tempfile).Decode(&fields); err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = EditIssue(owner, repo, number, fields)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
