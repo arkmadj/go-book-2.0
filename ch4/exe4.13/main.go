5353b5b17adbd6628733fbd4ab747f555c5b130a
arkmadj <ahmadjinadu@gmail.com>
2022-11-18 06:30:21 +0100

    Started OMDB

---
+++ ch4/exe4.13/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "path/filepath"
+
+const APIURL = "http://www.omdbapi.com/?"
+
+type Movie struct {
+	Title  string
+	Year   string
+	Poster string
+}
+
+func (m Movie) posterFilename() string {
+	ext := filepath.Ext(m.Poster)
+}
