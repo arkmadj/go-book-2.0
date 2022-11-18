b65f14242072d2a768ec3d2a0d0b1a242be6745b
arkmadj <ahmadjinadu@gmail.com>
2022-11-18 06:38:02 +0100

    Added get movies

--- ch4/exe4.13/main.go
+++ ch4/exe4.13/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/url"
 	"path/filepath"
 
 	"github.com/metal3d/go-slugify"
@@ -19,4 +22,22 @@ func (m Movie) posterFilename() string {
 	ext := filepath.Ext(m.Poster)
 	title := slugify.Marshal(m.Title)
 	return fmt.Sprintf("%s_(%s)%s", title, m.Year, ext)
+}
+
+func getMovie(title string) (movie Movie, err error) {
+	url_ := fmt.Sprintf("%st=%s", APIURL, url.QueryEscape(title))
+	resp, err := http.Get(url_)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("%d response from %s", resp.StatusCode, url_)
+		return
+	}
+	err = json.NewDecoder(resp.Body).Decode(&movie)
+	if err != nil {
+		return
+	}
+	return
 }
