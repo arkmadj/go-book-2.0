f3f6fdbd2a2c43c9cbbdfea13aec97151caa1e73
arkmadj <ahmadjinadu@gmail.com>
2022-11-18 07:40:53 +0100

    Added html template

---
+++ ch4/issueshtml/main.go
@@ -0,0 +1,23 @@
+package issueshtml
+
+import "html/template"
+
+var issueList = template.Must(template.New("issuelist").Parse(`
+<h1>{{.TotalCount}} issues </h1>
+<table>
+<tr style='text-align: left'>
+	<th>#</th>
+	<th>State</th>
+	<th>User</th>
+	<th>Title</th>
+</tr>
+{{range .Items}}
+<tr>
+	<td><a href='{{.HTMLURL}}'>{{.Number}}</a></td>
+	<td>{{.State}}</td>
+	<td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+	<td><a href='{{.HTMLURL}}'>{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>
+`))
