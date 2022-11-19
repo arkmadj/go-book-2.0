5eee55b87d9188b07d2a1b0f6f5afc0633df9caa
arkmadj <ahmadjinadu@gmail.com>
2022-11-19 22:56:11 +0100

    Added issue struct

--- ch4/exe4.14/github.go
+++ ch4/exe4.14/github.go
@@ -1,9 +1,21 @@
 package main
 
+import "time"
+
 const IssuesURL = "https://api.github.com/search/issues"
 const APIURL = "https://api.github.com"
 
 type IssuesSearchResult struct {
 	TotalCount int `json:"total_count"`
 	Items      []*Issue
+}
+
+type Issue struct {
+	Number    int
+	HTML      string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string
 }
