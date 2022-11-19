e02b1e3818889b062559cc3932612c4f2261ca7e
arkmadj <ahmadjinadu@gmail.com>
2022-11-19 23:37:20 +0100

    Added issue list trmplate

---
+++ ch4/exe4.14/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "html/template"
+
+var issueListTemplate = template.Must(template.New("issueList").Parse(`
+<h1>{{.Issues | len}} issues</h1>
+<table>
+<tr style='text-align: left'>
+<th>#</th>
+<th>State</th>
+<th>User</th>
+<th>Title</th>
+</tr>
+{{range .Issues}}
+<tr>
+<td><a href='{{.Cache}}'>{{.Number}}</td>
+<td>{{.State}}</td>
+<td><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></td>
+<td><a href='{{.CacheURL}}'>{{.Title}}</a></td>
+</tr>
+{{end}}
+</table>
+`))
