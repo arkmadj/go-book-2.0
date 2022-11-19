50d74267729732b018edcc2688c4ae71966077e7
arkmadj <ahmadjinadu@gmail.com>
2022-11-19 23:41:23 +0100

    Added issue template

--- ch4/exe4.14/main.go
+++ ch4/exe4.14/main.go
@@ -21,3 +21,14 @@ var issueListTemplate = template.Must(template.New("issueList").Parse(`
 {{end}}
 </table>
 `))
+
+var issueTemplates = template.Must(template.New("issue").Parse(`
+<h1>{{.Title}}</h1>
+<dl>
+	<dt>user</dt>
+	<dd><a href='{{.User.HTMLURL}}'>{{.User.Login}}</a></dd>
+	<dt>state</dt>
+	<dd>{{.State}}</dd>
+</dl>
+<p>{{.Body}}</p>
+`))
