27d5894f960586a8218586e34912f1d634211e11
arkmadj <ahmadjinadu@gmail.com>
2022-11-19 22:57:37 +0100

    Added type user

--- ch4/exe4.14/github.go
+++ ch4/exe4.14/github.go
@@ -12,10 +12,15 @@ type IssuesSearchResult struct {
 
 type Issue struct {
 	Number    int
-	HTML      string `json:"html_url"`
+	HTMLURL   string `json:"html_url"`
 	Title     string
 	State     string
 	User      *User
 	CreatedAt time.Time `json:"created_at"`
 	Body      string
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
 }
