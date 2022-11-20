da6c5b0e1dddc590c23d3103cb2724025555156d
arkmadj <ahmadjinadu@gmail.com>
2022-11-20 23:59:14 +0100

    added vist links

--- ch4/findlinks1/main.go
+++ ch4/findlinks1/main.go
@@ -17,3 +17,17 @@ func main() {
 		fmt.Println(link)
 	}
 }
+
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
