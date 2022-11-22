aa3b08e737c259308b39bd361edd23fba52a9190
arkmadj <ahmadjinadu@gmail.com>
2022-11-22 22:39:51 +0100

    Added outline

---
+++ ch4/outline/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ahmad/go-book-2.0/ch4/html"
+)
+
+func outline(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data)
+		fmt.Println(stack)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		outline(stack, c)
+	}
+}
