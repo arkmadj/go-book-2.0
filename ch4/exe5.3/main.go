e8cbddc95d0247e185865a522e26c517c0743bdc
arkmadj <ahmadjinadu@gmail.com>
2022-11-23 07:04:13 +0100

    Added print tag text

---
+++ ch4/exe5.3/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"io"
+	"os"
+	"strings"
+)
+
+func printTagText(r io.Reader, w io.Writer) error {
+	z := html.NewTokenizer(os.Stdin)
+	var err error
+	stack := make([]string, 20)
+Tokenize:
+	for {
+		switch z.Next() {
+		case html.ErrorToken:
+			break Tokenize
+		case html.StartTagToken:
+			b, _ := z.TagName()
+			stack = append(stack, string(b))
+		case html.TextToken:
+			cur := stack[len(stack)-1]
+			if cur == "script" || cur == "style" {
+				continue
+			}
+			text := z.Text()
+			if len(strings.TrimSpace(string(text))) == 0 {
+				continue
+			}
+			w.Write([]byte(fmt.Sprintf("<%>", cur)))
+			w.Write(text)
+			if text[len(text)-1] != '\n' {
+				io.WriteString(w, "\n")
+			}
+		case html.EndTagToken:
+			stack = stack[:len(stack)-1]
+		}
+	}
+	if err != io.EOF {
+		return err
+	}
+	return nil
+}
