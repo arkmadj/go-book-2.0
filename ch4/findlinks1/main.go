1003c39bba9b3eb6ce728a9e2f7f4e3e0471b850
arkmadj <ahmadjinadu@gmail.com>
2022-11-23 06:27:02 +0100

    Fixed typo

--- ch4/findlinks1/main.go
+++ ch4/findlinks1/main.go
@@ -21,7 +21,7 @@ func main() {
 func visit(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, a := range n.Attr {
-			if a.key == "href" {
+			if a.Key == "href" {
 				links = append(links, a.Val)
 			}
 		}
