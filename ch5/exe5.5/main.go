e1915b25c5f8345cb0517664b048f4cecd1b7ac6
arkmadj <ahmadjinadu@gmail.com>
2022-11-24 22:50:02 +0100

    Added counted words and images function

--- ch5/exe5.5/main.go
+++ ch5/exe5.5/main.go
@@ -18,6 +18,27 @@ func CountWordsAndImages(url string) (words, images int, err error) {
 		err = fmt.Errorf("Parsing HTML: %s", err)
 		return
 	}
-	words, images = CountWordsAndImages(doc)
+	words, images = countWordsAndImages(doc)
+	return
+}
+
+func countWordsAndImages(n *html.Node) (words, images int) {
+	u := make([]*html.Node, 0)
+	u = append(u, n)
+	for len(u) > 0 {
+		n = u[len(u)-1]
+		u = u[:len(u)-1]
+		switch n.Type {
+		case html.TextNode:
+			words += wordCount(n.Data)
+		case html.ElementNode:
+			if n.Data == "img" {
+				images++
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			u = append(u, c)
+		}
+	}
 	return
 }
