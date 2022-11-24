48d0c3b4dc260e510c90eda677f9bf107522fb76
arkmadj <ahmadjinadu@gmail.com>
2022-11-24 22:57:21 +0100

    Added const

---
+++ ch5/exe5.6/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "math"
+
+const (
+	width, height = 600, 320
+	cells         = 100
+	xyrange       = 30.0
+	xyscale       = width / 2 / xyrange
+	zscale        = height * 0.4
+	angle         = math.Pi / 6
+)
