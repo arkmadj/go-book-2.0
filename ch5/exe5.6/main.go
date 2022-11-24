36d76797461b9dc245bd387104d829b1a6dde9a3
arkmadj <ahmadjinadu@gmail.com>
2022-11-24 23:11:04 +0100

    Added main func

--- ch5/exe5.6/main.go
+++ ch5/exe5.6/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 const (
 	width, height = 600, 320
@@ -27,4 +30,20 @@ func corner(i, j int) (sx, sy float64) {
 	sx = width/2 + (x-y)*cos30*xyscale
 	sy = height/2 + (x+y)*sin30*xyscale - z*zscale
 	return
+}
+
+func main() {
+	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			fmt.Printf("<polygon points='%g,%g,%g,%g,%g,%g,%g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Printf("<svg/>")
 }
