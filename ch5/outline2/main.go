71f8562ce8366cf91f323985f78a5d2bca3cd17a
arkmadj <ahmadjinadu@gmail.com>
2022-11-26 22:22:56 +0100

    added forEachNode

--- ch5/outline2/main.go
+++ ch5/outline2/main.go
@@ -1,1 +1,17 @@
 package main
+
+import "github.com/ahmad/go-book-2.0/ch4/html"
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
