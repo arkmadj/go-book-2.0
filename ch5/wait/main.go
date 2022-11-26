691907ed2aec88953e482a0a93999edbbfb5476e
arkmadj <ahmadjinadu@gmail.com>
2022-11-26 22:01:09 +0100

    Fixed typos

--- ch5/wait/main.go
+++ ch5/wait/main.go
@@ -11,19 +11,19 @@ import (
 func WaitForServer(url string) error {
 	const timeout = 1 * time.Minute
 	deadline := time.Now().Add(timeout)
-	for tries := 0; time.Now().Before(deadline); tries++{
-		_, err : http.Head(url)
+	for tries := 0; time.Now().Before(deadline); tries++ {
+		_, err := http.Head(url)
 		if err == nil {
 			return nil
 		}
 		log.Printf("server not responding (%s); retrying...", err)
-		time.Sleep(time.Second << unit(tries))
+		time.Sleep(time.Second << uint(tries))
 	}
 	return fmt.Errorf("server %s failed to respond after %s", url, timeout)
 }
 
-func main(){
-	url := "http://www.proberker.com/"
+func main() {
+	var url string = "http://www.proberker.com/"
 	if err := WaitForServer(url); err != nil {
 		fmt.Fprintf(os.Stderr, "Site is down: %v\n", err)
 	}
