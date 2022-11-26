c5f62a135498c17414c3e33763e1b0961217eb43
arkmadj <ahmadjinadu@gmail.com>
2022-11-26 22:25:00 +0100

    Added start element

--- ch5/outline2/main.go
+++ ch5/outline2/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/ahmad/go-book-2.0/ch4/html"
+import (
+	"fmt"
+
+	"github.com/ahmad/go-book-2.0/ch4/html"
+)
 
 func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	if pre != nil {
@@ -13,5 +17,14 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 
 	if post != nil {
 		post(n)
+	}
+}
+
+var depth int
+
+func startElement(n *html.Node) {
+	if n.Type == html.ElementNode {
+		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		depth++
 	}
 }
