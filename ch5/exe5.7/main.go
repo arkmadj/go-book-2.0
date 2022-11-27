6946c1bc4ff69572b3b5c0d4f69f37186dc90d41
arkmadj <ahmadjinadu@gmail.com>
2022-11-27 23:56:22 +0100

    Added startElement

--- ch5/exe5.7/main.go
+++ ch5/exe5.7/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/ahmad/go-book-2.0/ch4/html"
 )
@@ -50,4 +51,26 @@ func (pp PrettyPrinter) forEachNode(n *html.Node, pre, post func(n *html.Node)) 
 func (pp PrettyPrinter) printf(format string, args ...interface{}) {
 	_, err := fmt.Fprintf(pp.w, format, args...)
 	pp.err = err
+}
+
+func (pp PrettyPrinter) startElement(n *html.Node) {
+	end := ">"
+	if n.FirstChild == nil {
+		end = "/>"
+	}
+
+	attrs := make([]string, 0, len(n.Attr))
+	for _, a := range n.Attr {
+		attrs = append(attrs, fmt.Sprintf(`%s="%s"`, a.Key, a.Val))
+	}
+
+	attrStr := ""
+	if len(n.Attr) > 0 {
+		attrStr = " " + strings.Join(attrs, " ")
+	}
+
+	name := n.Data
+
+	pp.printf("%*s<%s%s%s\n", depth*2, "", name, attrStr, end)
+	depth++
 }
