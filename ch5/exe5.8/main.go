b3c506a87e992b5c5cceab9d77490d919eac0d09
arkmadj <ahmadjinadu@gmail.com>
2022-11-28 22:52:33 +0100

    Added forEachElement

--- ch5/exe5.8/main.go
+++ ch5/exe5.8/main.go
@@ -16,3 +16,26 @@ func ElementByID(n *html.Node, id string) *html.Node {
 	}
 	return forEachElement(n, pre, nil)
 }
+
+func forEachElement(n *html.Node, pre, post func(n *html.Node) bool) *html.Node {
+	u := make([]*html.Node, 0)
+	u = append(u, n)
+	for len(u) > 0 {
+		n = u[0]
+		u = u[1:]
+		if pre != nil {
+			if !pre(n) {
+				return n
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			u = append(u, c)
+		}
+		if post != nil {
+			if !post(n) {
+				return n
+			}
+		}
+	}
+	return nil
+}
