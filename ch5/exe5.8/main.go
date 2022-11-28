8f14f2b861c61d89c9d88f436c536c5f406c8489
arkmadj <ahmadjinadu@gmail.com>
2022-11-28 22:49:17 +0100

    Added element by id

---
+++ ch5/exe5.8/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "golang.org/x/net/html"
+
+func ElementBtID(n *html.Node, id string) *html.Node {
+	pre := func(n *html.Node) bool {
+		if n.Type != html.ElementNode {
+			return true
+		}
+		for _, a := range n.Attr {
+			if a.Key == "id" && a.Val == id {
+				return false
+			}
+		}
+		return true
+	}
+	return forEachElement(n, pre, nil)
+}
