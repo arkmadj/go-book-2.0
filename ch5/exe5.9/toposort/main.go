53b5c8bd4a8cfe3e36e1bf6390e4dcac1be4826a
arkmadj <ahmadjinadu@gmail.com>
2022-11-29 23:27:34 +0100

    Added prereqs map

---
+++ ch5/exe5.9/toposort/main.go
@@ -0,0 +1,13 @@
+package main
+
+var prereqs = map[string][]string{
+	"algorithms":            {"data structures"},
+	"calculus":              {"linear algebra"},
+	"compliers":             {"data structures", "formal languages", "computer organization"},
+	"data structures":       {"discrete math"},
+	"databases":             {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
