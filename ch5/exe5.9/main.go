b24813e6c48c2473859f41f9077b6e9b102cd90c
arkmadj <ahmadjinadu@gmail.com>
2022-11-29 22:50:14 +0100

    Added expand function

--- ch5/exe5.9/main.go
+++ ch5/exe5.9/main.go
@@ -1,5 +1,20 @@
 package main
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 var pattern = regexp.MustCompile(`\$\w+|\${\w+}`)
+
+func expand(s string, f func(string) string) string {
+	wrapper := func(s string) string {
+		if strings.HasPrefix(s, "${") {
+			s = s[2 : len(s)-1]
+		} else {
+			s = s[1:]
+		}
+		return f(s)
+	}
+	return pattern.ReplaceAllStringFunc(s, wrapper)
+}
