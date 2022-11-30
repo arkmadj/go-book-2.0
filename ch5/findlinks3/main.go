b28636f40d420bc8efd75bb978831e9a6dc734f7
arkmadj <ahmadjinadu@gmail.com>
2022-11-30 22:35:31 +0100

    Added crawl

--- ch5/findlinks3/main.go
+++ ch5/findlinks3/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/ahmad/go-book-2.0/ch5/links"
+)
+
 func breadthFirst(f func(item string) []string, worklist []string) {
 	seen := make(map[string]bool)
 	for len(worklist) > 0 {
@@ -12,4 +19,13 @@ func breadthFirst(f func(item string) []string, worklist []string) {
 			}
 		}
 	}
+}
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
 }
