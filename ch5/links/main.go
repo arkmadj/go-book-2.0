7e47f78f4036cc34f18f9f565deca492c63a604d
arkmadj <ahmadjinadu@gmail.com>
2022-11-30 22:39:33 +0100

    Added for each node

--- ch5/links/main.go
+++ ch5/links/main.go
@@ -41,3 +41,17 @@ func Extract(url string) ([]string, error) {
 	forEachNode(doc, visitNode, nil)
 	return links, nil
 }
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
