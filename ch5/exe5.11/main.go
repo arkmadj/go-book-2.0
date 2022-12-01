3481e2525ddaa9416efba15b624cf43d46d1ba40
arkmadj <ahmadjinadu@gmail.com>
2022-12-01 22:40:29 +0100

    Added toposort

--- ch5/exe5.11/main.go
+++ ch5/exe5.11/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 var prereqs = map[string][]string{
@@ -45,4 +46,33 @@ func index(s string, slice []string) (int, error) {
 		}
 	}
 	return 0, fmt.Errorf("not found")
+}
+
+func topoSort(m map[string][]string) (order []string, err error) {
+	resolved := make(map[string]bool)
+	var visitAll func([]string, []string)
+
+	visitAll = func(items []string, parents []string) {
+		for _, v := range items {
+			vResolved, seen := resolved[v]
+			if seen && !vResolved {
+				start, _ := index(v, parents)
+				err = fmt.Errorf("cycle: %s", strings.Join(append(parents[start:], v), " -> "))
+			}
+			if !seen {
+				resolved[v] = false
+				visitAll(m[v], append(parents, v))
+				resolved[v] = true
+				order = append(order, v)
+			}
+		}
+	}
+
+	for k := range m {
+		if err != nil {
+			return nil, err
+		}
+		visitAll([]string{k}, nil)
+	}
+	return order, nil
 }
