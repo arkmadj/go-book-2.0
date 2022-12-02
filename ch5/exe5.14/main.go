905534154148c2e3a44949de4ed274b1f5bd4fea
arkmadj <ahmadjinadu@gmail.com>
2022-12-02 23:31:11 +0100

    Added prereqs

---
+++ ch5/exe5.14/main.go
@@ -0,0 +1,20 @@
+package main
+
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
