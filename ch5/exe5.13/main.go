6b189c5dedcaf997605d0bd64f3bb53e1bd642be
arkmadj <ahmadjinadu@gmail.com>
2022-12-02 23:24:04 +0100

    Added save function

--- ch5/exe5.13/main.go
+++ ch5/exe5.13/main.go
@@ -1,5 +1,14 @@
 package maain
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+)
+
 func breadthFirst(f func(item string) []string, worklist []string) {
 	seen := make(map[string]bool)
 	for len(worklist) > 0 {
@@ -15,3 +24,47 @@ func breadthFirst(f func(item string) []string, worklist []string) {
 }
 
 var origHost string
+
+func save(rawurl string) error {
+	url, err := url.Parse(rawurl)
+	if err != nil {
+		return fmt.Errorf("bad url: %s", err)
+	}
+	if origHost == "" {
+		origHost = url.Host
+	}
+	if origHost != url.Host {
+		return nil
+	}
+	dir := url.Host
+	var filename string
+	if filepath.Ext(filename) == "" {
+		dir = filepath.Join(dir, url.Path)
+		filename = filepath.Join(dir, "index.html")
+	} else {
+		dir = filepath.Join(dir, filepath.Dir(url.Path))
+		filename = url.Path
+	}
+	err = os.MkdirAll(dir, 0777)
+	if err != nil {
+		return err
+	}
+	resp, err := http.Get(rawurl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+	err = file.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
