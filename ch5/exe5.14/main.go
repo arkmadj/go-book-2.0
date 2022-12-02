bc6a89692327e341fa7af54d277e053f18e8e04d
arkmadj <ahmadjinadu@gmail.com>
2022-12-02 23:34:25 +0100

    Added breadfirst function

--- ch5/exe5.14/main.go
+++ ch5/exe5.14/main.go
@@ -18,3 +18,17 @@ var prereqs = map[string][]string{
 	"operating systems":     {"data structures", "computer organization"},
 	"programming languages": {"data structures", "computer organization"},
 }
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
