089a7c86dfbe274ff1a3469b0b38900e8085653a
arkmadj <ahmadjinadu@gmail.com>
2022-12-03 20:33:00 +0100

    Added min func

--- ch5/exe5.15/main.go
+++ ch5/exe5.15/main.go
@@ -12,3 +12,16 @@ func max(nums ...int) int {
 	}
 	return m
 }
+
+func min(nums ...int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	m := nums[0]
+	for _, n := range nums {
+		if n < m {
+			m = n
+		}
+	}
+	return m
+}
