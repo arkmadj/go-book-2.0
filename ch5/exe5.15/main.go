cf96ed0857482d1493ce0d5ebaef5ccff6873246
arkmadj <ahmadjinadu@gmail.com>
2022-12-03 20:37:27 +0100

    Added min2 function

--- ch5/exe5.15/main.go
+++ ch5/exe5.15/main.go
@@ -38,10 +38,20 @@ func max2(first int, nums ...int) int {
 	return m
 }
 
+func min2(first int, nums ...int) int {
+	m := first
+	for _, n := range nums {
+		if n < m {
+			m = n
+		}
+	}
+	return m
+}
+
 func main() {
 	values := []int{1, 2, 3, 4, 5}
 	fmt.Println(min(values...))
 	fmt.Println(max(values...))
-	fmt.Println(min2(values...))
+	fmt.Println(min2(-1, values...))
 	fmt.Println(max2(6, values...))
 }
