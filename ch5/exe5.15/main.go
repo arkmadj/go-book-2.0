e94359b46bb05f691bc67fdce0ce3e56ba6c4db9
arkmadj <ahmadjinadu@gmail.com>
2022-12-03 20:36:05 +0100

    Added max2 function

--- ch5/exe5.15/main.go
+++ ch5/exe5.15/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func max(nums ...int) int {
 	if len(nums) == 0 {
 		return 0
@@ -24,4 +26,22 @@ func min(nums ...int) int {
 		}
 	}
 	return m
+}
+
+func max2(first int, nums ...int) int {
+	m := first
+	for _, n := range nums {
+		if n > m {
+			m = n
+		}
+	}
+	return m
+}
+
+func main() {
+	values := []int{1, 2, 3, 4, 5}
+	fmt.Println(min(values...))
+	fmt.Println(max(values...))
+	fmt.Println(min2(values...))
+	fmt.Println(max2(6, values...))
 }
