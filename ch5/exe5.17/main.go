960f3b36e25e14c8089eb73f8cb317f812c20785
arkmadj <ahmadjinadu@gmail.com>
2022-12-03 21:09:58 +0100

    Added foreEacElementNode func

--- ch5/exe5.17/main.go
+++ ch5/exe5.17/main.go
@@ -22,3 +22,26 @@ func ElementsByTag(n *html.Node, tags ...string) []*html.Node {
 	forEachElementNode(n, pre, nil)
 	return nodes
 }
+
+func forEachElementNode(n *html.Node, pre, post func(n *html.Node) bool) *html.Node {
+	u := make([]*html.Node, 0)
+	u = append(u, n)
+	for len(u) > 0 {
+		n = u[0]
+		u = u[1:]
+		if pre != nil {
+			if !pre(n) {
+				return n
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			u = append(u, c)
+		}
+		if post != nil {
+			if !post(n) {
+				return n
+			}
+		}
+	}
+	return nil
+}
