d4f2ffcbb1238d58893cbb955e3bde6fe7653a84
arkmadj <ahmadjinadu@gmail.com>
2022-12-05 23:33:33 +0100

    Added f function

--- ch5/defer2/main.go
+++ ch5/defer2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"runtime"
 )
@@ -9,6 +10,12 @@ func printStack() {
 	var buf [4096]byte
 	n := runtime.Stack(buf[:], false)
 	os.Stdout.Write(buf[:n])
+}
+
+func f(x int) {
+	fmt.Printf("f(%d)\n", x+0/x)
+	defer fmt.Printf("defer %d\n", x)
+	f(x - 1)
 }
 
 func main() {
