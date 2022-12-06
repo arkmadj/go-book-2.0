59b9b41e70d1932c38323728264ff4d8bed335e3
arkmadj <ahmadjinadu@gmail.com>
2022-12-06 23:39:53 +0100

    Added foreachnode func

--- ch5/title3/main.go
+++ ch5/title3/main.go
@@ -34,3 +34,17 @@ func soleTitle(doc *html.Node) (title string, err error) {
 	}
 	return title, nil
 }
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
