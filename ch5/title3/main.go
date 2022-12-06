f21395aa0b2eb71cedd344e921f3a672af25e52d
arkmadj <ahmadjinadu@gmail.com>
2022-12-06 23:32:52 +0100

    Added defer

--- ch5/title3/main.go
+++ ch5/title3/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "golang.org/x/net/html"
+import (
+	"fmt"
+
+	"golang.org/x/net/html"
+)
 
 func soleTitle(doc *html.Node) (title string, err error) {
 	type bailout struct{}
+
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+		case bailout{}:
+			err = fmt.Errorf("multiple title elements")
+		default:
+			panic(p)
+		}
+	}()
+	return title, nil
 }
