77470ad3e704129c90abc646e352775a97e46a29
arkmadj <ahmadjinadu@gmail.com>
2022-12-08 22:01:55 +0100

    Added main func

--- ch6/geometry/geometry.go
+++ ch6/geometry/geometry.go
@@ -1,6 +1,9 @@
 package geometry
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 type Point struct{ X, Y float64 }
 
@@ -10,4 +13,11 @@ func Distance(p, q Point) float64 {
 
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+func main() {
+	p := Point{1, 2}
+	q := Point{4, 6}
+	fmt.Println(Distance(p, q))
+	fmt.Println(p.Distance(q))
 }
