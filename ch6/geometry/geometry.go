bb33e5624f71af52bbcc4c75d10c0b36e550a034
arkmadj <ahmadjinadu@gmail.com>
2022-12-08 21:59:30 +0100

    Added distance method

--- ch6/geometry/geometry.go
+++ ch6/geometry/geometry.go
@@ -7,3 +7,7 @@ type Point struct{ X, Y float64 }
 func Distance(p, q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
