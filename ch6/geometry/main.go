2ebc42b8254fa5bf0168d179efb064bf44f2fa47
arkmadj <ahmadjinadu@gmail.com>
2022-12-08 22:17:37 +0100

    Added perim

--- ch6/geometry/main.go
+++ ch6/geometry/main.go
@@ -32,4 +32,13 @@ func main() {
 	q := Point{4, 6}
 	fmt.Println(Distance(p, q))
 	fmt.Println(p.Distance(q))
+
+	perim := Path{
+		{1, 1},
+		{5, 1},
+		{5, 4},
+		{1, 1},
+	}
+
+	fmt.Println(perim.Distance())
 }
