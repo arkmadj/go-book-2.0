dfddd3e8476f837742ed40f9f4c6859bab76787a
arkmadj <ahmadjinadu@gmail.com>
2022-12-08 22:07:34 +0100

    Added  distance method for path

--- ch6/geometry/main.go
+++ ch6/geometry/main.go
@@ -17,6 +17,16 @@ func (p Point) Distance(q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
 }
 
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+
 func main() {
 	p := Point{1, 2}
 	q := Point{4, 6}
