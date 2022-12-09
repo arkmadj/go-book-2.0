e05cb72c17c562ff5426c3507ff3e216c66e06af
arkmadj <ahmadjinadu@gmail.com>
2022-12-09 23:36:07 +0100

    ADDED get func

--- ch6/urlvalues/main.go
+++ ch6/urlvalues/main.go
@@ -2,12 +2,12 @@ package main
 
 type Values map[string][]string
 
-// func (v Values) Get(key string) string {
-// 	if vs := v[key]; len(vs) > 0 {
-// 		return vs[0]
-// 	}
-// 	return ""
-// }
+func (v Values) Get(key string) string {
+	if vs := v[key]; len(vs) > 0 {
+		return vs[0]
+	}
+	return ""
+}
 
 // func (v Values) Add(key, value string) {
 // 	v[key] = append(v[key], value)
