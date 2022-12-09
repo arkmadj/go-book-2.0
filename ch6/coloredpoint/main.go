8ab8e24fdbba95b203c3076d99273e18d3fec682
arkmadj <ahmadjinadu@gmail.com>
2022-12-09 23:56:59 +0100

    Added mian

--- ch6/coloredpoint/main.go
+++ ch6/coloredpoint/main.go
@@ -18,4 +18,17 @@ func main() {
 	fmt.Println(cp.Point.X)
 	cp.Point.Y = 2
 	fmt.Println(cp.Y)
+
+	m = nil
+	fmt.Println(m.Get("item"))
+	m.Add("item", "3")
+
+	red := color.RGBA{255, 0, 0, 255}
+	blue := color.RGBA{255, 0, 0, 255}
+	var p = ColoredPoint{Point{1, 1}, red}
+	var q = ColoredPoint{Point{1, 1}, blue}
+	fmt.Println(p.Distance(q.Point))
+	p.ScaleBy(2)
+	q.ScaleBy(2)
+	fmt.Println(p.Distance(q.Point))
 }
