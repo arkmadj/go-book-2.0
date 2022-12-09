e31ec1bf881ac17fb7662a4f90706a9539a8d9bc
arkmadj <ahmadjinadu@gmail.com>
2022-12-09 23:51:24 +0100

    Adde main func

--- ch6/coloredpoint/main.go
+++ ch6/coloredpoint/main.go
@@ -1,10 +1,21 @@
 package main
 
-import "image/color"
+import (
+	"fmt"
+	"image/color"
+)
 
 type Point struct{ X, Y float64 }
 
 type ColoredPoint struct {
 	Point
 	Color color.RGBA
+}
+
+func main() {
+	var cp ColoredPoint
+	cp.X = 1
+	fmt.Println(cp.Point.X)
+	cp.Point.Y = 2
+	fmt.Println(cp.Y)
 }
