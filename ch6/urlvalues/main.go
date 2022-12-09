b706fbaafeac857b71ac8b4d3b01794c483820fd
arkmadj <ahmadjinadu@gmail.com>
2022-12-09 23:55:24 +0100

    Added main func

--- ch6/urlvalues/main.go
+++ ch6/urlvalues/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"net/url"
 )
 
@@ -31,4 +32,13 @@ func main() {
 	m = nil
 	fmt.Println(m.Get("item"))
 	m.Add("item", "3")
+
+	red := color.RGBA{255, 0, 0, 255}
+	blue := color.RGBA{255, 0, 0, 255}
+	var p = ColoredPoint{Point{1, 1}, red}
+	var q = ColoredPoint{Point{1, 1}, blue}
+	fmt.Println(p.Distance(q.Point))
+	p.ScaleBy(2)
+	q.ScaleBy(2)
+	fmt.Println(p.Distance(q.Point))
 }
