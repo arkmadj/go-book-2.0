fea064e66a7fa4f34a0a12c6bd31939b422f8ed4
arkmadj <ahmadjinadu@gmail.com>
2022-12-10 23:35:04 +0100

    Added string func

--- ch6/intset/main.go
+++ ch6/intset/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+)
+
 type IntSet struct {
 	words []uint64
 }
@@ -25,4 +30,24 @@ func (s *IntSet) UnionWith(t *IntSet) {
 			s.words = append(s.words, tword)
 		}
 	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
 }
