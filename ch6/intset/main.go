1e334cabe88a86b610322308553cc9559613b37a
arkmadj <ahmadjinadu@gmail.com>
2022-12-10 23:30:49 +0100

    Added union with func

--- ch6/intset/main.go
+++ ch6/intset/main.go
@@ -16,3 +16,13 @@ func (s *IntSet) Add(x int) {
 	}
 	s.words[word] |= 1 << bit
 }
+
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
