f595da447acb9cdeb93cdcebb01ffde611556c12
arkmadj <ahmadjinadu@gmail.com>
2022-12-10 23:17:05 +0100

    Added sub func

--- ch6/geometry/main.go
+++ ch6/geometry/main.go
@@ -32,7 +32,8 @@ func (p *Point) ScaleBy(factor float64) {
 	p.Y *= factor
 }
 
-func (p Point) Add(q Point) Point { return Point{p.X + q.X, p.Y + q.y} }
+func (p Point) Add(q Point) Point { return Point{p.X + q.X, p.Y + q.Y} }
+func (p Point) Sub(q Point) Point { return Point{p.X - q.X, p.Y - q.Y} }
 
 // func (path Path) TranslateBy(offset Point, add bool){
 // 	var op func(p, q Point)Point
