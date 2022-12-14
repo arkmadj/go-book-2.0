c930bb0c69be1da3dd4fef3a2bed454f7dda7cc3
arkmadj <ahmadjinadu@gmail.com>
2022-12-14 07:53:14 +0100

    Added main func

--- ch6/exe6.3/main.go
+++ ch6/exe6.3/main.go
@@ -122,3 +122,19 @@ func (s *IntSet) String() string {
 	buf.WriteByte('}')
 	return buf.String()
 }
+
+func main() {
+	var x, y IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String())
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String())
+
+	x.UnionWith(&y)
+	fmt.Println(x.String())
+	fmt.Println(x.Has(9), x.Has(123))
+}
