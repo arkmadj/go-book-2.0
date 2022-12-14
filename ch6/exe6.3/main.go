571c52063d1c6fef23b004e50a709107a81384e9
arkmadj <ahmadjinadu@gmail.com>
2022-12-14 07:13:56 +0100

    Added has func

--- ch6/exe6.3/main.go
+++ ch6/exe6.3/main.go
@@ -3,3 +3,8 @@ package amin
 type IntSet struct {
 	words []uint64
 }
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
