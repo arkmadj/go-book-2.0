005c82b561a88d9e6f1bd85d486bb67508841086
arkmadj <ahmadjinadu@gmail.com>
2022-12-14 07:47:34 +0100

    Added String func

--- ch6/exe6.3/main.go
+++ ch6/exe6.3/main.go
@@ -1,5 +1,10 @@
 package amin
 
+import (
+	"bytes"
+	"fmt"
+)
+
 type IntSet struct {
 	words []uint64
 }
@@ -96,4 +101,24 @@ func (s *IntSet) Copy() *IntSet {
 	new.words = make([]uint64, len(s.words))
 	copy(new.words, s.words)
 	return new
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i*j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
 }
