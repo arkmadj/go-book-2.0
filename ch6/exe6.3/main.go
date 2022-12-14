cdd8ca8c3a7f5d0cf3487cf8a80552583663e675
arkmadj <ahmadjinadu@gmail.com>
2022-12-14 07:35:56 +0100

    Added symmeticDiffernce func

--- ch6/exe6.3/main.go
+++ ch6/exe6.3/main.go
@@ -52,3 +52,15 @@ func (s *IntSet) DiffernceWith(t *IntSet) {
 		}
 	}
 }
+
+func (s *IntSet) SymmetricDifference(t *IntSet){
+	for i, tword := range t.words{
+		if i < len(s.words){
+			s.words[i] ^= tword
+		}else{
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func 
