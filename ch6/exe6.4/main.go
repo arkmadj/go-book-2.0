bfb3ffb1c01c82e51fb7c6bf73bb6a063dbfbbce
arkmadj <ahmadjinadu@gmail.com>
2022-12-15 22:01:10 +0100

    Added Has func

--- ch6/exe6.4/main.go
+++ ch6/exe6.4/main.go
@@ -3,3 +3,8 @@ package main
 type IntSet struct {
 	words []uint64
 }
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
