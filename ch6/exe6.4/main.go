e2c046ac826d84a8b93edbd92652fc2158bcedf3
arkmadj <ahmadjinadu@gmail.com>
2022-12-15 22:08:08 +0100

    Added intersect with func

--- ch6/exe6.4/main.go
+++ ch6/exe6.4/main.go
@@ -32,3 +32,13 @@ func (s *IntSet) UnionWith(t *IntSet) {
 		}
 	}
 }
+
+func (s *IntSet) IntersectWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
