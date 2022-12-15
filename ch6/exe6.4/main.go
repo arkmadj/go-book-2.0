d65df669fd70f6a6e81b0c6ba03f642c2da9d84e
arkmadj <ahmadjinadu@gmail.com>
2022-12-15 22:11:58 +0100

    Added difference with func

--- ch6/exe6.4/main.go
+++ ch6/exe6.4/main.go
@@ -43,6 +43,16 @@ func (s *IntSet) IntersectWith(t *IntSet) {
 	}
 }
 
+func (s *IntSet) DiffernceWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
 func (s *IntSet) SymmetricDifference(t *IntSet) {
 	for i, tword := range t.words {
 		if i < len(s.words) {
