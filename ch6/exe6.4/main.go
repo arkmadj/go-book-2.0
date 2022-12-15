f8b1dec0e18bed29bb25d2bc9e762f59caa63f67
arkmadj <ahmadjinadu@gmail.com>
2022-12-15 22:24:46 +0100

    Added Elem func

--- ch6/exe6.4/main.go
+++ ch6/exe6.4/main.go
@@ -122,3 +122,15 @@ func (s *IntSet) String() string {
 	buf.WriteByte('}')
 	return buf.String()
 }
+
+func (s *IntSet) Elems() []int {
+	e := make([]int, 0)
+	for i, word := range s.words {
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				e = append(e, i*64+j)
+			}
+		}
+	}
+	return e
+}
