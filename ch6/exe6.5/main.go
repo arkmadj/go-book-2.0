972d92d2b62b6f5ff89f41b8e6272dad74948169
arkmadj <ahmadjinadu@gmail.com>
2022-12-16 22:47:22 +0100

    added has func

--- ch6/exe6.5/main.go
+++ ch6/exe6.5/main.go
@@ -5,3 +5,8 @@ const wordSize = 32 << (^uint(0) >> 63)
 type IntSet struct {
 	words []uint
 }
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/wordSize, uint(x%wordSize)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
