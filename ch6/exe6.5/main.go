87d15d20d0c9b757279ebf8514be01d71ecfd1c7
arkmadj <ahmadjinadu@gmail.com>
2022-12-16 22:53:55 +0100

    Added difference with func

--- ch6/exe6.5/main.go
+++ ch6/exe6.5/main.go
@@ -34,3 +34,13 @@ func (s *IntSet) UnionWith(t *IntSet) {
 		}
 	}
 }
+
+func (s *IntSet) DiffernceWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
