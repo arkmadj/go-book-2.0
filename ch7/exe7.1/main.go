086f3b57f0b183a869cc754bd0948592519e0d27
arkmadj <ahmadjinadu@gmail.com>
2022-12-17 23:29:42 +0100

    Added write func for linecounter

--- ch7/exe7.1/main.go
+++ ch7/exe7.1/main.go
@@ -3,3 +3,12 @@ package main
 type LineCounter struct {
 	lines int
 }
+
+func (c *LineCounter) Write(p []byte) (n int, err error) {
+	for _, b := range p {
+		if b == '\n' {
+			c.lines++
+		}
+	}
+	return len(p), nil
+}
