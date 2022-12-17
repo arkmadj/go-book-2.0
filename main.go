4e46a44301ca68287887508a1d172eb66882a485
arkmadj <ahmadjinadu@gmail.com>
2022-12-17 23:20:58 +0100

    Added len in main

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
+import "fmt"
+
 type ByteCounter int
 
 func (c *ByteCounter) Write(p []byte) (int, error) {
 	*c += ByteCounter(len(p))
 	return len(p), nil
+}
+
+func main() {
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c)
 }
