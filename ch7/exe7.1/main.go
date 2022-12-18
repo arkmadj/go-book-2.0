e49d7a6c38d4808a9995653425eb25fd389f0cc8
arkmadj <ahmadjinadu@gmail.com>
2022-12-18 20:43:52 +0100

    Fixed typo

--- ch7/exe7.1/main.go
+++ ch7/exe7.1/main.go
@@ -103,7 +103,7 @@ func main() {
 	pw := [][]byte{
 		[]byte("My name is Ahmad Jin"),
 		[]byte("adu Adekunle. "),
-		[]byte(" I am a software engineer from Lagos, Nigeria."),
+		[]byte(" I'm a software engineer from Lagos, Nigeria."),
 		[]byte(" Nice to meet you."),
 	}
 
