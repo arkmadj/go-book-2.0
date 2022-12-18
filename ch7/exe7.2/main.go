5831a9ca617c355a6e09fe358bd6c72d7d90c0ac
arkmadj <ahmadjinadu@gmail.com>
2022-12-18 20:33:38 +0100

    Added write func for byteCounter

--- ch7/exe7.2/main.go
+++ ch7/exe7.2/main.go
@@ -6,3 +6,14 @@ type byteCounter struct {
 	w       io.Writer
 	written int64
 }
+
+func (c *byteCounter) Write(p []byte) (n int, err error) {
+	n, err = c.w.Write(p)
+	c.written += int64(n)
+	return
+}
+
+func CountingWriter(w io.Writer) (io.Writer, *int64) {
+	c := &byteCounter{w, 0}
+	return c, &c.written
+}
