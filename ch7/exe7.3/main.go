2b4474d1b54abafbc4f98ed208c4e652e965dcb9
arkmadj <ahmadjinadu@gmail.com>
2022-12-20 23:32:21 +0100

    Added add func

--- ch7/exe7.3/main.go
+++ ch7/exe7.3/main.go
@@ -21,3 +21,17 @@ func appendValues(values []int, t *tree) []int {
 	}
 	return values
 }
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
