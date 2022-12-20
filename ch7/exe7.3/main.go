56040ca99c8863f22f3ef5ac8248ecbde72fe07d
arkmadj <ahmadjinadu@gmail.com>
2022-12-20 23:29:04 +0100

    Added appendvalues func

--- ch7/exe7.3/main.go
+++ ch7/exe7.3/main.go
@@ -12,3 +12,11 @@ func Sort(values []int) {
 	}
 	appendValues(values[:0], root)
 }
+
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.values)
+		values = appendValues(values, t.right)
+	}
+}
