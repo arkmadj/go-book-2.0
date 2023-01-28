a3def63244d46f751baffd42f7ed5322648531ad
arkmadj <ahmadjinadu@gmail.com>
2023-01-28 23:14:20 +0100

    Fixed typo

--- ch7/exe7.4/main.go
+++ ch7/exe7.4/main.go
@@ -27,7 +27,7 @@ func NewReader(s string) io.Reader {
 }
 
 func main() {
-	s := "hi thsssdssssadesdre"
+	s := "hi thsssdsssssdadesdre"
 	b := &bytes.Buffer{}
 	n, err := b.ReadFrom(NewReader(s))
 	if err != nil {
