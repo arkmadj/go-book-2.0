ed8f696aa1723f7b67c5698465230b41df3833b0
arkmadj <ahmadjinadu@gmail.com>
2023-01-29 09:32:32 +0100

    Added set flag

--- ch7/tempconv/main.go
+++ ch7/tempconv/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Celsius float64
 type Fahrenheit float64
 
@@ -13,4 +15,22 @@ func CToF(c Celsius) Fahrenheit {
 
 func FToC(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
+}
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+
+	switch unit {
+	case "C", "ºC":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F", "ºF":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+
+	}
+	return fmt.Errorf("invalid temperature %q", s)
 }
