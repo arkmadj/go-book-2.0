bd2599615e53f55a8d529f7f55f1c0e440bea4f7
arkmadj <ahmadjinadu@gmail.com>
2023-01-29 07:59:03 +0100

    Added read func

--- ch7/exe7.5/main.go
+++ ch7/exe7.5/main.go
@@ -6,3 +6,12 @@ type limitReader struct {
 	r        io.Reader
 	n, limit int
 }
+
+func (r *limitReader) Read(p []byte) (n int, err error) {
+	n, err = r.r.Read(p[:r.limit])
+	r.n += n
+	if r.n > r.limit {
+		err = io.EOF
+	}
+	return
+}
