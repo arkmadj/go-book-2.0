0185a4494e791bee2920b2efff13f162af103c8f
arkmadj <ahmadjinadu@gmail.com>
2023-01-29 09:28:23 +0100

    Added C->F and C->F funcs

--- ch7/tempconv/main.go
+++ ch7/tempconv/main.go
@@ -6,3 +6,11 @@ type Fahrenheit float64
 type celsiusFlag struct {
 	Celsius
 }
+
+func CToF(c Celsius) Fahrenheit {
+	return Fahrenheit(c*9/5 + 32)
+}
+
+func FToC(f Fahrenheit) Celsius {
+	return Celsius((f - 32) * 5 / 9)
+}
