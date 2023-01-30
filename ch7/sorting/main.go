f28b4ed13a3cffd4bc026fb47cabea1bbf58af26
arkmadj <ahmadjinadu@gmail.com>
2023-01-30 07:20:20 +0100

    Added track list

--- ch7/sorting/main.go
+++ ch7/sorting/main.go
@@ -3,13 +3,24 @@ package main
 import "time"
 
 type Track struct {
-	Title string
+	Title  string
 	Artist string
-	Album string
-	Year int
+	Album  string
+	Year   int
 	Length time.Duration
 }
 
 var tracks = []*Track{
-	{"Go"}
+	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
 }
