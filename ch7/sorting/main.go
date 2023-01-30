fc80c041db8c384993459ff44fe3ebf4148e2430
arkmadj <ahmadjinadu@gmail.com>
2023-01-30 07:20:35 +0100

    Added length func

--- ch7/sorting/main.go
+++ ch7/sorting/main.go
@@ -16,3 +16,11 @@ var tracks = []*Track{
 	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
 	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
 }
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
