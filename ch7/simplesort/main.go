3cb5b075a1a1f190da0a0453af4378db88d45b09
arkmadj <ahmadjinadu@gmail.com>
2023-01-30 07:14:31 +0100

    Adde main func

--- ch7/simplesort/main.go
+++ ch7/simplesort/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 type StringSlice []string
 
 func (p StringSlice) Len() int {
@@ -12,4 +17,11 @@ func (p StringSlice) Less(i, j int) bool {
 
 func (p StringSlice) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
+}
+
+func main() {
+	names := []string{"ahmad", "jinadu", "adekunle", "zultan"}
+	fmt.Println(names)
+	sort.Sort(StringSlice(names))
+	fmt.Println(names)
 }
