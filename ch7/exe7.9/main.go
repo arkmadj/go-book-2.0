a3107e92f2600f2d758b271bf8f395d35942cc4e
arkmadj <ahmadjinadu@gmail.com>
2023-02-01 06:11:01 +0100

    Added people var

--- ch7/exe7.9/main.go
+++ ch7/exe7.9/main.go
@@ -1,1 +1,13 @@
 package main
+
+import {
+	"github.com/ahmad/go-book-2.0/exe7.8"
+}
+
+var people = []column.Person{
+	{"Alice",20},
+	{"Bob",12},
+	{"Bob", 20},
+	{"Alice",12},
+	{"Alex",18},
+}
--- ch7/sorting/main.go
+++ ch7/sorting/main.go
@@ -1,4 +1,4 @@
-package column
+package main
 
 import (
 	"fmt"
