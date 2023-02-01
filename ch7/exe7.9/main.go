88974b1f5a20287882203b41b3486762aa9c3690
arkmadj <ahmadjinadu@gmail.com>
2023-02-01 06:18:41 +0100

    Added html template

--- ch7/exe7.9/main.go
+++ ch7/exe7.9/main.go
@@ -1,13 +1,35 @@
 package main
 
-import {
-	"github.com/ahmad/go-book-2.0/exe7.8"
-}
+import (
+	"html/template"
+
+	column "github.com/ahmad/go-book-2.0/ch7/exe7.8"
+)
 
 var people = []column.Person{
-	{"Alice",20},
-	{"Bob",12},
+	{"Alice", 20},
+	{"Bob", 12},
 	{"Bob", 20},
-	{"Alice",12},
-	{"Alex",18},
+	{"Alice", 12},
+	{"Alex", 18},
 }
+
+var html = template.Must(template.New("people").Parse(`
+<html>
+	<body>
+		<table>
+			<tr>
+				<th href="?sort=name">name</th>
+				<th><a href="?sort=age">age</a></th>
+			</tr>
+			{{range .}}
+			<tr>
+				<td>{{.Name}}</td>
+				<td>{{.Age}}</td>
+			</tr>
+			{{end}}
+		</table>
+	</body>
+</html>
+
+`))
