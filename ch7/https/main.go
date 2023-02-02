f5c2cd2a87ed484ba8ad52ce384892824cd44d0a
arkmadj <ahmadjinadu@gmail.com>
2023-02-02 07:34:52 +0100

    Added price func

--- ch7/https/main.go
+++ ch7/https/main.go
@@ -19,6 +19,17 @@ func (db database) list(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
 func main() {
 	db := database{"shoes"}
 }
