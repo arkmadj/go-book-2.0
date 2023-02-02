e7632889eb8463059d439f77df72b5b2bb33ed3a
arkmadj <ahmadjinadu@gmail.com>
2023-02-02 06:54:01 +0100

    Addded server

--- ch7/http1/main.go
+++ ch7/http1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -17,4 +18,9 @@ func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	for item, price := range db {
 		fmt.Fprintf(w, "%s: %s\n", item, price)
 	}
+}
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	log.Fatal(http.ListenAndServe("localhost:8085", db))
 }
