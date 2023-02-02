41ecbe4e47e0e648f0e48dad186d7bbcd46280b2
arkmadj <ahmadjinadu@gmail.com>
2023-02-02 07:13:45 +0100

    Added case /price

--- ch7/http2/main.go
+++ ch7/http2/main.go
@@ -20,6 +20,15 @@ func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		for item, price := range db {
 			fmt.Fprintf(w, "%s: %s\n", item, price)
 		}
+	case "/price":
+		item := req.URL.Query().Get("item")
+		price, ok := db[item]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "no such item: %q\n", item)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", price)
 	}
 }
 
