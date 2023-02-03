32242c46935866c9a148c2159053e24a824f5389
arkmadj <ahmadjinadu@gmail.com>
2023-02-03 06:10:35 +0100

    Fixede typo

--- ch7/exe7.11/main.go
+++ ch7/exe7.11/main.go
@@ -1,8 +1,31 @@
 package main
 
-import "sync"
+import (
+	"net/http"
+	"strconv"
+	"sync"
+)
 
 type PriceDB struct {
 	sync.Mutex
-	dp map[string]int
+	d map[string]int
+}
+
+func (p *PriceDB) Create(w http.ResponseWriter, r *http.Request) {
+	item := r.FormValue("item")
+	if item == "" {
+		http.Error(w, "No item given", http.StatusBadRequest)
+		return
+	}
+
+	priceStr := r.FormValue("price")
+	price, err := strconv.Atoi(priceStr)
+	if err != nil {
+		http.Error(w, "No integer price given", http.StatusBadGateway)
+		return
+	}
+
+	if _, ok := p.db[item]; ok {
+
+	}
 }
