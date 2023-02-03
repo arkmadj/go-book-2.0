8f43683c867802650de4b80906751fb352da9cde
arkmadj <ahmadjinadu@gmail.com>
2023-02-03 07:23:14 +0100

    Added evel func for unary type

--- ch7/eval/eval.go
+++ ch7/eval/eval.go
@@ -1,5 +1,7 @@
 package eval
 
+import "fmt"
+
 type Env map[Var]float64
 
 func (v Var) Eval(env Env) float64 {
@@ -8,4 +10,14 @@ func (v Var) Eval(env Env) float64 {
 
 func (l literal) Eval(env Env) float64 {
 	return float64(l)
+}
+
+func (u unary) Eval(env Env) float64 {
+	switch u.op {
+	case '+':
+		return +u.x.Eval(env)
+	case '-':
+		return -u.x.Eval(env)
+	}
+	panic(fmt.Sprintf("un supported unary operator: %q", u.op))
 }
