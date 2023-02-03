c973c0f36875ee818aa873bca81d6b2ef674672e
arkmadj <ahmadjinadu@gmail.com>
2023-02-03 08:23:48 +0100

    Added testEval func

--- ch7/eval/eval_test.go
+++ ch7/eval/eval_test.go
@@ -27,5 +27,14 @@ func TestEval(t *testing.T) {
 			prevExpr = test.expr
 		}
 		expr, err := Parse(test.expr)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		got := fmt.Sprintf("%.6g", expr.Eval(test.env))
+		fmt.Printf("\t%v => %s\n", test.env, got)
+		if got != test.want {
+			t.Errorf("%s.Eval() in %s = %q, want %q\n", test.expr, test.env, got, test.want)
+		}
 	}
 }
