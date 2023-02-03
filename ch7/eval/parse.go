38b8c13e36708bbeb9e7a026d0eb5f685da3f0da
arkmadj <ahmadjinadu@gmail.com>
2023-02-03 07:53:11 +0100

    Added describe func

--- ch7/eval/parse.go
+++ ch7/eval/parse.go
@@ -1,6 +1,7 @@
 package eval
 
 import (
+	"fmt"
 	"text/scanner"
 )
 
@@ -17,3 +18,25 @@ func (lex *lexer) text() string {
 }
 
 type lexPanic string
+
+func (lex *lexer) describe() string {
+	switch lex.token {
+	case scanner.EOF:
+		return "end of file"
+	case scanner.Ident:
+		return fmt.Sprintf("identifier %s", lex.text())
+	case scanner.Int, scanner.Float:
+		return fmt.Sprintf("number %s", lex.text())
+	}
+	return fmt.Sprintf("%q", rune(lex.token))
+}
+
+// func precedence(op rune) int {
+// 	switch op {
+// 	case '*', '/':
+// 		return 2
+// 	case '+', '-':
+// 		return 1
+// 	}
+// 	return
+// }
