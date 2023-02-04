14bd5fa67d613fdb0001e33ae609127ab8855810
arkmadj <ahmadjinadu@gmail.com>
2023-02-04 21:33:01 +0100

    Added Check unary func

--- ch7/eval/check.go
+++ ch7/eval/check.go
@@ -1,5 +1,10 @@
 package eval
 
+import (
+	"fmt"
+	"strings"
+)
+
 func (v Var) Check(vars map[Var]bool) error {
 	vars[v] = true
 	return nil
@@ -7,4 +12,11 @@ func (v Var) Check(vars map[Var]bool) error {
 
 func (literal) Check(vars map[Var]bool) error {
 	return nil
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.op) {
+		return fmt.Errorf("unexpected unary op %q", u.op)
+	}
+	return u.x.Check(vars)
 }
