da73fcdc6d5e3a43e864cc77b113ed6cffd9196f
arkmadj <ahmadjinadu@gmail.com>
2023-02-04 22:31:51 +0100

    Added surface func

--- ch7/surface/surface.go
+++ ch7/surface/surface.go
@@ -1,6 +1,10 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"io"
+	"math"
+)
 
 const (
 	width, height = 600, 320
@@ -21,4 +25,20 @@ func corner(f func(x, y) float64, i, j int) (float64, float64) {
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
 	return sx, sy
+}
+
+func surface(w io.Writer, f func(x, y float64) float64) {
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(f, i+1, j)
+			bx, by := corner(f, i, j)
+			cx, cy := corner(f, i, j+1)
+			dx, dy := corner(f, i+1, j+1)
+			fmt.Fprintf(w, "<polygon points='%g,%g,%g,%g,%g,%g,%g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
+		}
+	}
+	fmt.Fprintln(w, "</svg>")
 }
