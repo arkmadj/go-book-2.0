9f6bf653f2bab59fa2023594ceba2404e5476958
arkmadj <ahmadjinadu@gmail.com>
2023-02-04 22:36:05 +0100

    Added parseAndCheck func

--- ch7/surface/surface.go
+++ ch7/surface/surface.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io"
 	"math"
+
+	"github.com/ahmad/go-book-2.0/ch7/eval"
 )
 
 const (
@@ -41,4 +43,24 @@ func surface(w io.Writer, f func(x, y float64) float64) {
 		}
 	}
 	fmt.Fprintln(w, "</svg>")
+}
+
+func parseAndCheck(s string) (eval.Expr, error) {
+	if s == "" {
+		return nil, fmt.Errorf("empty expression")
+	}
+	expr, err := eval.Parse(s)
+	if err != nil {
+		return nil, err
+	}
+	vars := make(map[eval.Var]bool)
+	if err := expr.Check(vars); err != nil {
+		return nil, err
+	}
+	for v := range vars {
+		if v != "x" && v != "y" && v != "r" {
+			return nil, fmt.Errorf("undefined variable: %s", v)
+		}
+	}
+	return expr, nil
 }
