0f51147190c7b9b01cfdf642aca218e25ec64805
arkmadj <ahmadjinadu@gmail.com>
2023-02-04 22:23:49 +0100

    Added corner func

--- ch7/surface/surface.go
+++ ch7/surface/surface.go
@@ -11,3 +11,14 @@ const (
 )
 
 var sin30, cos30 = 0.5, math.Sqrt(3.0 / 4.0)
+
+func corner(f func(x, y) float64, i, j int) (float64, float64) {
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	z := f(x, y)
+
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy
+}
