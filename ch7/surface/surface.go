80f58bc53a253ea9f3ed4e2eb8a32f79c0b25c5c
arkmadj <ahmadjinadu@gmail.com>
2023-02-04 22:20:20 +0100

    Added sin30 and cos30

--- ch7/surface/surface.go
+++ ch7/surface/surface.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 const (
 	width, height = 600, 320
 	cells         = 100
@@ -7,3 +9,5 @@ const (
 	xyscale       = width / 2 / xyrange
 	zscale        = height * 0.4
 )
+
+var sin30, cos30 = 0.5, math.Sqrt(3.0 / 4.0)
