7f98db8c7a8b3b461abc953aa6a50fe2da2eeac4
arkmadj <ahmadjinadu@gmail.com>
2023-02-05 05:57:52 +0100

    Added call string func

--- ch7/eval/string.go
+++ ch7/eval/string.go
@@ -1,6 +1,9 @@
 package eval
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 func (v Var) String() string {
 	return string(v)
@@ -16,4 +19,18 @@ func (u unary) String() string {
 
 func (b binary) String() string {
 	return fmt.Sprintf("(%s %s %s)", b.x.String(), string(b.op), b.y.String())
+}
+
+func (c call) String() string {
+	b := &bytes.Buffer{}
+	b.WriteString(c.fn)
+	b.WriteString("(")
+	for i, a := range c.args {
+		if i != 0 {
+			b.WriteString(", ")
+		}
+		b.WriteString(a.String())
+	}
+	b.WriteString(")")
+	return b.String()
 }
