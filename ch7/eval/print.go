0d5e75336d9bef924d0d52626728be37017b0c60
arkmadj <ahmadjinadu@gmail.com>
2023-02-05 06:59:23 +0100

    Added write func

--- ch7/eval/print.go
+++ ch7/eval/print.go
@@ -1,7 +1,40 @@
 package eval
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+)
 
 func Format(e Expr) string {
 	var buf bytes.Buffer
+}
+
+func write(buf *bytes.Buffer, e Expr) {
+	switch e := e.(type) {
+	case literal:
+		fmt.Fprintf(buf, "%g", e)
+	case Var:
+		fmt.Fprintf(buf, "%s", e)
+	case unary:
+		fmt.Fprintf(buf, "(%c", e.op)
+		write(buf, e.x)
+		buf.WriteByte(')')
+	case binary:
+		buf.WriteByte('(')
+		write(buf, e.x)
+		fmt.Fprintf(buf, " %c ", e.op)
+		write(buf, e.y)
+		buf.WriteByte(')')
+	case call:
+		fmt.Fprintf(buf, "%s(", e.fn)
+		for I, arg := range e.args {
+			if i > 0 {
+				buf.WriteString(", ")
+			}
+			write(buf, arg)
+		}
+		buf.WriteByte(')')
+	default:
+		panic(fmt.Sprintf("unknown Expr: %T", e))
+	}
 }
