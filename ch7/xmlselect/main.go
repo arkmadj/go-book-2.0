8262b2c8cf959b5e7a71c54db70d9e2a45f31eed
arkmadj <ahmadjinadu@gmail.com>
2023-02-06 07:14:55 +0100

    Added main func

--- ch7/xmlselect/main.go
+++ ch7/xmlselect/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
 func containsAll(x, y []string) bool {
 	for len(y) <= len(x) {
 		if len(y) == 0 {
@@ -11,4 +19,28 @@ func containsAll(x, y []string) bool {
 		x = x[1:]
 	}
 	return false
+}
+
+func main() {
+	dec := xml.NewDecoder(os.Stdin)
+	var stack []string
+	for {
+		tok, err := dec.Token()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Fprintf(os.Stderr, "xmlselect: %v\n", err)
+			os.Exit(1)
+		}
+		switch tok := tok.(type) {
+		case xml.StartElement:
+			stack = append(stack, tok.Name.Local)
+		case xml.EndElement:
+			stack = stack[:len(stack)-1]
+		case xml.CharData:
+			if containsAll(stack, os.Args[1:]) {
+				fmt.Printf("%s: %s\n", strings.Join(stack, " "), tok)
+			}
+		}
+	}
 }
