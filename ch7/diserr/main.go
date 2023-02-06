2268285694dac3fd597195a519d5339f15f71bda
arkmadj <ahmadjinadu@gmail.com>
2023-02-06 06:15:43 +0100

    Added PathError struct

--- ch7/diserr/main.go
+++ ch7/diserr/main.go
@@ -7,6 +7,12 @@ import (
 
 var ErrNotExist = errors.New("file does not exist")
 
+type PathError struct {
+	Op   string
+	Path string
+	Err  string
+}
+
 func IsNotExist(err error) bool {
 	if pe, ok := err.(*PathError); ok {
 		err = pe.Err
