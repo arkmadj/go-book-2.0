05ff897caa8e705caed87cb2864c0326198ab67b
arkmadj <ahmadjinadu@gmail.com>
2023-02-06 06:14:04 +0100

    Added IsNotExist func

--- ch7/diserr/main.go
+++ ch7/diserr/main.go
@@ -1,5 +1,15 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"syscall"
+)
 
 var ErrNotExist = errors.New("file does not exist")
+
+func IsNotExist(err error) bool {
+	if pe, ok := err.(*PathError); ok {
+		err = pe.Err
+	}
+	return err == syscall.ENOENT || err == ErrNotExist
+}
