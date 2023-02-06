86341789e6c5481159f230e90858e0505a894eb3
arkmadj <ahmadjinadu@gmail.com>
2023-02-06 07:10:20 +0100

    Added containsAll func

--- ch7/xmlselect/main.go
+++ ch7/xmlselect/main.go
@@ -1,1 +1,14 @@
 package main
+
+func containsAll(x, y []string) bool {
+	for len(y) <= len(x) {
+		if len(y) == 0 {
+			return true
+		}
+		if x[0] == y[0] {
+			y = y[1:]
+		}
+		x = x[1:]
+	}
+	return false
+}
