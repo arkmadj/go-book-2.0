0bc2935733252b6638cbfa5104a312f546658cde
arkmadj <ahmadjinadu@gmail.com>
2023-02-12 07:13:00 +0100

    Added fib func

--- ch8/spinner/main.go
+++ ch8/spinner/main.go
@@ -14,9 +14,9 @@ func spinner(delay time.Duration) {
 	}
 }
 
-// func fib(x int) int {
-// 	if x < 2 {
-// 		return x
-// 	}
-// 	return fib(x - 1) + fib(x -2)
-// }
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
