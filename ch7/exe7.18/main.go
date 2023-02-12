4a216b57798043bb40cbfe5d00f8f86133bd7ee5
arkmadj <ahmadjinadu@gmail.com>
2023-02-12 06:31:36 +0100

    Added parse func

--- ch7/exe7.18/main.go
+++ ch7/exe7.18/main.go
@@ -36,3 +36,34 @@ func visit(n Node, w io.Writer, depth int) {
 		panic(fmt.Sprintf("got %T", n))
 	}
 }
+
+func parse(r io.Reader) (Node, error) {
+	dec := xml.NewDecoder(r)
+	var stack []*Element
+	var root Node
+	for {
+		tok, err := dec.Token()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+		switch tok := tok.(type) {
+		case xml.StartElement:
+			el := &Element{tok.Name, tok.Attr, nil}
+			if len(stack) == 0 {
+				root = el
+			} else {
+				parent := stack[len(stack)-1]
+				parent.Children = append(parent.Children, el)
+			}
+			stack = append(stack, el)
+		case xml.EndElement:
+			stack = stack[:len(stack)-1]
+		case xml.CharData:
+			parent := stack[len(stack)-1]
+			parent.Children = append(parent.Children, CharData(tok))
+		}
+	}
+	return root, nil
+}
