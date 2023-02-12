48892496a2fc955aa86364d19f3eb2e71dcfd071
arkmadj <ahmadjinadu@gmail.com>
2023-02-12 08:30:31 +0100

    Added main func

--- ch8/exe8.1/clockwall.go
+++ ch8/exe8.1/clockwall.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
 
 type clock struct {
@@ -19,5 +23,32 @@ func (c *clock) watch(w io.Writer, r io.Reader) {
 	fmt.Println(c.name, "done")
 	if s.Err() != nil {
 		log.Printf("can't read from %s: %s", c.name, s.Err())
+	}
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Fprintf(os.Stderr, "usage: clockwall NAME=HOST ...")
+		os.Exit(1)
+	}
+	clocks := make([]*clock, 0)
+	for _, a := range os.Args[1:] {
+		fields := strings.Split(a, "=")
+		if len(fields) != 2 {
+			fmt.Fprintf(os.Stderr, "bad arg: %s\n", a)
+			os.Exit(1)
+		}
+		clocks = append(clocks, &clock{fields[0], fields[1]})
+	}
+	for _, c := range clocks {
+		conn, err := net.Dial("tcp", c.host)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer conn.Close()
+		go c.watch(os.Stdout, conn)
+	}
+	for {
+		time.Sleep(time.Minute)
 	}
 }
