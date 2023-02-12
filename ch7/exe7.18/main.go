7e297f001fb692e0804e12082d3cbc47d59f0355
arkmadj <ahmadjinadu@gmail.com>
2023-02-12 06:16:02 +0100

    Added visit func

--- ch7/exe7.18/main.go
+++ ch7/exe7.18/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"encoding/xml"
+	"fmt"
+	"io"
 )
 
 type Node interface{}
@@ -18,4 +20,18 @@ type Element struct {
 func (n *Element) String() string {
 	b := &bytes.Buffer{}
 	v
+}
+
+func visit(n Node, w io.Writer, depth int) {
+	switch n := n.(type) {
+	case *Element:
+		fmt.Fprintf(w, "%*s%s %s\n", depth*2, "", n.Type.Local, n.Attr)
+		for _, c := range n.Children {
+			visit(c, w, depth+1)
+		}
+	case CharData:
+		fmt.Fprintf(w, "%*s%q\n", depth*2, "", n)
+	default:
+		panic(fmt.Sprint("got %T", n))
+	}
 }
