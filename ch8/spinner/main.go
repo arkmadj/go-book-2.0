764e92824664f56ceb2491db7fd142a9a1226139
arkmadj <ahmadjinadu@gmail.com>
2023-02-12 07:12:48 +0100

    Added spinner func

--- ch8/spinner/main.go
+++ ch8/spinner/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+// func fib(x int) int {
+// 	if x < 2 {
+// 		return x
+// 	}
+// 	return fib(x - 1) + fib(x -2)
+// }
