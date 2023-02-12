bd8ac06b226322fb82dd941e80f9dc5e2cea1b81
arkmadj <ahmadjinadu@gmail.com>
2023-02-12 08:21:44 +0100

    Added watch func

--- ch8/exe8.1/clockwall.go
+++ ch8/exe8.1/clockwall.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+)
+
 type clock struct {
 	name, host string
+}
+
+func (c *clock) watch(w io.Writer, r io.Reader) {
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		fmt.Fprintf(w, "%s: %s\n", c.name, s.Text())
+	}
+	fmt.Println(c.name, "done")
+	if s.Err() != nil {
+		log.Printf("can't read from %s: %s", c.name, s.Err())
+	}
 }
