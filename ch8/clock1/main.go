cbacbb0142f4c5fb18f4febede2dc0903e478f9c
arkmadj <ahmadjinadu@gmail.com>
2023-02-12 07:30:37 +0100

    Added handle func

--- ch8/clock1/main.go
+++ ch8/clock1/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"io"
+	"net"
+	"time"
+)
+
+func handleCoon(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
