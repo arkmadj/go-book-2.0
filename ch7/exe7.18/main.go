82df4d3f3b3c4906528b3bf19f2ee1db7b5230bf
arkmadj <ahmadjinadu@gmail.com>
2023-02-12 06:11:32 +0100

    Added CharData type

--- ch7/exe7.18/main.go
+++ ch7/exe7.18/main.go
@@ -1,3 +1,16 @@
 package main
 
 type Node interface{}
+
+type CharData string
+
+// type Element struct {
+// 	Type     xml.Name
+// 	Attr     []xml.Attr
+// 	Children []Node
+// }
+
+// func (n *Element) String() string {
+// 	b := &bytes.Buffer{}
+// 	v
+// }
