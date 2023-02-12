f799d6d02f500baa078198eb091554a3ebaba778
arkmadj <ahmadjinadu@gmail.com>
2023-02-12 07:34:12 +0100

    Added main func

--- ch8/clock1/main.go
+++ ch8/clock1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"log"
 	"net"
 	"time"
 )
@@ -14,5 +15,20 @@ func handleConn(c net.Conn) {
 			return
 		}
 		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		handleConn(conn)
 	}
 }
