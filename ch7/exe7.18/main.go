3d1312515aaed377e959f774dc722a457a289cec
arkmadj <ahmadjinadu@gmail.com>
2023-02-12 06:11:48 +0100

    Added Element type

--- ch7/exe7.18/main.go
+++ ch7/exe7.18/main.go
@@ -1,14 +1,16 @@
 package main
 
+import "encoding/xml"
+
 type Node interface{}
 
 type CharData string
 
-// type Element struct {
-// 	Type     xml.Name
-// 	Attr     []xml.Attr
-// 	Children []Node
-// }
+type Element struct {
+	Type     xml.Name
+	Attr     []xml.Attr
+	Children []Node
+}
 
 // func (n *Element) String() string {
 // 	b := &bytes.Buffer{}
