3a8841d3d55dcb8e1d8929847de04d26bab238ca
arkmadj <ahmadjinadu@gmail.com>
2023-02-13 05:58:36 +0100

    Added hostPortToFTP func

--- ch8/exe8.2/main.go
+++ ch8/exe8.2/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"strconv"
+)
 
 type conn struct {
 	rw           net.Conn
@@ -13,4 +17,22 @@ type conn struct {
 
 func NewConn(cmdConn net.Conn) *conn {
 	return &conn{rw: cmdConn}
+}
+
+func hostPortToFTP(hostport string) (addr string, err error) {
+	host, portStr, err := net.SplitHostPort(hostport)
+	if err != nil {
+		return "", err
+	}
+	ipAddr, err := net.ResolveIPAddr("ip4", host)
+	if err != nil {
+		return "", err
+	}
+	port, err := strconv.ParseInt(portStr, 10, 64)
+	if err != nil {
+		return "", err
+	}
+	ip := ipAddr.IP.To4()
+	s := fmt.Sprintf("%d, %d, %d, %d, %d, %d", ip[0], ip[1], ip[2], ip[3], port/256, port%256)
+	return s, nil
 }
