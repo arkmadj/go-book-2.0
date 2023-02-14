5eb28d03431519ba916b3999b7fe1c5964fb8625
arkmadj <ahmadjinadu@gmail.com>
2023-02-14 06:20:27 +0100

    Added main func

--- ch8/reverb1/main.go
+++ ch8/reverb1/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
+	"log"
 	"net"
 	"strings"
 	"time"
 )
+
+var port = flag.Int("port", 8080, "listen")
 
 func echo(c net.Conn, shout string, delay time.Duration) {
 	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
@@ -22,4 +26,20 @@ func handleConn(c net.Conn) {
 		echo(c, input.Text(), 1*time.Second)
 	}
 	c.Close()
+}
+
+func main() {
+	flag.Parse()
+	listener, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
 }
