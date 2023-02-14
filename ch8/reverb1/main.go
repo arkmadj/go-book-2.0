9d16362bee87382bbc6cda50ccbf2f7c4652894b
arkmadj <ahmadjinadu@gmail.com>
2023-02-14 06:17:14 +0100

    Added handleconn func

--- ch8/reverb1/main.go
+++ ch8/reverb1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"strings"
@@ -13,4 +14,12 @@ func echo(c net.Conn, shout string, delay time.Duration) {
 	fmt.Fprintln(c, "\t", shout)
 	time.Sleep(delay)
 	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	c.Close()
 }
