45a9b4bff81bf9913009e7d7143ed60f1b07662a
arkmadj <ahmadjinadu@gmail.com>
2023-02-14 06:15:37 +0100

    Added echo func

---
+++ ch8/reverb1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
