af692daa493a7431ba76dcfb5f12c767a6f15a29
arkmadj <ahmadjinadu@gmail.com>
2023-02-15 07:04:20 +0100

    Addded naturals func

--- ch8/pipeline2/main.go
+++ ch8/pipeline2/main.go
@@ -1,1 +1,13 @@
 package main
+
+func main() {
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	go func() {
+		for x := 0; x < 100; x++ {
+			naturals <- x
+		}
+		close(naturals)
+	}()
+}
