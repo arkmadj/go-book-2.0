2251403cfd63ac10f886da272ea927a58a338a8a
arkmadj <ahmadjinadu@gmail.com>
2023-02-15 06:16:20 +0100

    Added main func

--- ch8/netcat3/main.go
+++ ch8/netcat3/main.go
@@ -3,10 +3,28 @@ package main
 import (
 	"io"
 	"log"
+	"net"
+	"os"
 )
 
 func mustCopy(dst io.Writer, src io.Reader) {
 	if _, err := io.Copy(dst, src); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn)
+		log.Println("done")
+		done <- struct{}{}
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+	<-done
 }
