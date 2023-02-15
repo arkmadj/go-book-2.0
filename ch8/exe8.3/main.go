68e6d03b95ecbcafc195e271b8f41abbf196d979
arkmadj <ahmadjinadu@gmail.com>
2023-02-15 06:36:45 +0100

    Added must copy func

--- ch8/exe8.3/main.go
+++ ch8/exe8.3/main.go
@@ -1,3 +1,17 @@
 package main
 
+import (
+	"io"
+	"log"
+)
+
 func main() {}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		if err == io.EOF {
+			return
+		}
+		log.Fatal(err)
+	}
+}
