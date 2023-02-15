106880c52e1587e8c4e55402d77ed6954200cd4b
arkmadj <ahmadjinadu@gmail.com>
2023-02-15 06:40:27 +0100

    Added main func

--- ch8/exe8.3/main.go
+++ ch8/exe8.3/main.go
@@ -3,9 +3,28 @@ package main
 import (
 	"io"
 	"log"
+	"net"
+	"os"
 )
 
-func main() {}
+func main() {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn)
+		log.Println("done")
+		done <- struct{}{}
+	}()
+	mustCopy(conn, os.Stdin)
+	<-done
+}
 
 func mustCopy(dst io.Writer, src io.Reader) {
 	if _, err := io.Copy(dst, src); err != nil {
