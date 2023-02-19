15aeb9a2c448dbc3999d8e64d87fb3ccecce3edf
arkmadj <ahmadjinadu@gmail.com>
2023-02-19 11:58:56 +0100

    Fixded typo

--- ch8/exe8.5/main.go
+++ ch8/exe8.5/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"image"
 	"image/color"
 	"math"
 	"math/cmplx"
+	"runtime"
+	"sync"
+	"time"
 )
 
 const (
@@ -11,7 +15,32 @@ const (
 	width, height          = 1024, 1024
 )
 
-func newtow(z complex128) color.Color {
+func main() {
+	workers := runtime.GOMAXPROCS(-1)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	start := time.Now()
+	wg := sync.WaitGroup{}
+	rows := make(chan int, height)
+	for row := 0; row < height; row++ {
+		rows <- row
+	}
+	close(rows)
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			for py := range rows {
+				y := float64(py)/height*(ymax-ymin) + ymin
+				for px := 0; px < width; px++ {
+					x := float64(px)/width*(xmax-xmin) + xmin
+					z := complex(x, y)
+					img.Set(px, py, new)
+				}
+			}
+		}()
+	}
+}
+
+func newton(z complex128) color.Color {
 	iterations := 37
 	for n := uint8(0); int(n) < iterations; n++ {
 		z -= (z - 1/(z*z*z)) / 4
