cd2143e02bb61203d4efbbfc44306355edf3bb9e
arkmadj <ahmadjinadu@gmail.com>
2023-02-19 11:13:53 +0100

    Added mustCopy func

--- exe8.4/main.go
+++ exe8.4/main.go
@@ -23,3 +23,12 @@ func main() {
 		done <- struct{}{}
 	}()
 }
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		if err == io.EOF {
+			return
+		}
+		log.Fatal(err)
+	}
+}
