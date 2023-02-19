f2429fec78592267fc79e808694945eac5ea0636
arkmadj <ahmadjinadu@gmail.com>
2023-02-19 12:18:14 +0100

    added exe8.5

--- ch8/exe8.5/main.go
+++ ch8/exe8.5/main.go
@@ -57,7 +57,7 @@ func newton(z complex128) color.Color {
 	for n := uint8(0); int(n) < iterations; n++ {
 		z -= (z - 1/(z*z*z)) / 4
 		if cmplx.Abs(cmplx.Pow(z, 4)-1) < 1e-6 {
-			return color.Gray{255 - uint8(math.Log(float64(n))/math.Log(float64(iterations+0))*244)}
+			return color.Gray{255 - uint8(math.Log(float64(n))/math.Log(float64(iterations+0))*255)}
 		}
 	}
 	return color.Black
