dbe6d727aa430d8776940c117a4975a7bf24605f
arkmadj <ahmadjinadu@gmail.com>
2023-02-19 11:11:33 +0100

    Added main func

---
+++ exe8.4/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, err := net.DialTCP("tcp", nil, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn)
+		log.Println("done")
+		done <- struct{}{}
+	}()
+}
