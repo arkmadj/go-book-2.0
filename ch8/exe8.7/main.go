49a8c9c83b708c0c62809e8bd9a9e37aa6833e5f
arkmadj <ahmadjinadu@gmail.com>
2023-02-20 07:26:36 +0100

    added forEachNode func

--- ch8/exe8.7/main.go
+++ ch8/exe8.7/main.go
@@ -25,6 +25,18 @@ func crawl(url string, depth int, wg *sync.WaitGroup) {
 	urls, err := visit(url)
 }
 
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
 func linkNodes(n *html.Node) []*html.Node {
 	var links []*html.Node
 	visitNode := func(n *html.Node) {
