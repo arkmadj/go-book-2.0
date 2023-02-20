307d3862776fee87083f20c52a4b916e1106ffcc
arkmadj <ahmadjinadu@gmail.com>
2023-02-20 07:12:25 +0100

    added crawl func

--- ch8/exe8.7/main.go
+++ ch8/exe8.7/main.go
@@ -10,3 +10,10 @@ var maxDepth int
 var seen = make(map[string]bool)
 var seenLock = sync.Mutex{}
 var base *url.URL
+
+func crawl(url string, depth int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	tokens <- struct{}{}
+	urls, err := visit(url)
+}
