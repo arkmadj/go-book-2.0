afb47e94d5946cc333dee9504103e3433c6ef05c
arkmadj <ahmadjinadu@gmail.com>
2023-02-20 07:50:22 +0100

    added save func

--- ch8/exe8.7/main.go
+++ ch8/exe8.7/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"path/filepath"
 	"strings"
 	"sync"
 
@@ -130,4 +133,39 @@ func visit(rawurl string) (urls []string, err error) {
 	}
 	err = save(resp, body)
 	return urls, err
+}
+
+func save(resp *http.Response, body io.Reader) error {
+	u := resp.Request.URL
+	filename := filepath.Join(u.Host, u.Path)
+	if filepath.Ext(u.Path) == "" {
+		filename = filepath.Join(u.Host, u.Path, "index.html")
+	}
+	err := os.MkdirAll(filepath.Dir(filename), 0777)
+	if err != nil {
+		return err
+	}
+	fmt.Println("filename:", filename)
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	if body != nil {
+		_, err := io.Copy(file, body)
+	} else {
+		_, err = io.Copy(file, resp.Body)
+	}
+	if err != nil {
+		log.Printf("save: ", err)
+	}
+
+	err = file.Close()
+	if err != nil {
+		log.Printf("save: ", err)
+	}
+	return nil
+}
+
+func main() {
+	flag.IntVar()
 }
