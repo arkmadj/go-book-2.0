6c4170ca8d9b5105450b7009a83bcdb2811bb882
arkmadj <ahmadjinadu@gmail.com>
2023-02-20 06:37:10 +0100

    added crawl func

--- ch8/exe8.6/findlinks.go
+++ ch8/exe8.6/findlinks.go
@@ -1,8 +1,36 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/ahmad/go-book-2.0/ch5/links"
+)
 
 var tokens = make(chan struct{}, 20)
 var maxDepth int
 var seen = make(map[string]bool)
 var seenLock = sync.Mutex{}
+
+func crawl(url string, depth int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(depth, url)
+	if depth >= maxDepth {
+		return
+	}
+	tokens <- struct{}{}
+	list, err := links.Extract(url)
+	<-tokens
+	if err != nil {
+		log.Print(err)
+	}
+	for _, link := range list {
+		seenLock.Lock()
+		if seen[link] {
+			seenLock.Unlock()
+			wg.Add(1)
+			go crawl(link, depth+1, wg)
+		}
+	}
+}
