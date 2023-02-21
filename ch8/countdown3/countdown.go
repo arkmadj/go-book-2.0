13ca3faae13bcd4f4fc15616ebb455df7894b078
arkmadj <ahmadjinadu@gmail.com>
2023-02-21 06:46:22 +0100

    added main func

--- ch8/countdown3/countdown.go
+++ ch8/countdown3/countdown.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
@@ -11,6 +12,18 @@ func main() {
 		os.Stdin.Read(make([]byte, 1))
 		abort <- struct{}{}
 	}()
+
+	fmt.Println("Commencing countdown. Press return to abort")
+	tick := time.Tick(1 * time.Second)
+	for countdown := 10; countdown > 0; countdown-- {
+		fmt.Println(countdown)
+		select {
+		case <-tick:
+		case <-abort:
+			fmt.Println("Launch aborted!")
+			return
+		}
+	}
 }
 
 func launch() {
