85e282f417841006e787669090def45834a09294
arkmadj <ahmadjinadu@gmail.com>
2023-02-21 06:51:35 +0100

    added echo func

--- ch8/exe8.8/reverb.go
+++ ch8/exe8.8/reverb.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
