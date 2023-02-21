7c11b04f10b8f35e03b2d68812acdea6f57539ed
arkmadj <ahmadjinadu@gmail.com>
2023-02-21 06:53:25 +0100

    added scan func

--- ch8/exe8.8/reverb.go
+++ ch8/exe8.8/reverb.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"net"
 	"strings"
 	"sync"
@@ -15,4 +18,14 @@ func echo(c net.Conn, shout string, delay time.Duration, wg *sync.WaitGroup) {
 	fmt.Fprintln(c, "\t", shout)
 	time.Sleep(delay)
 	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func scan(r io.Reader, lines chan<- string) {
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		lines <- s.Text()
+	}
+	if s.Err() != nil {
+		log.Print("scan: ", s.Err())
+	}
 }
