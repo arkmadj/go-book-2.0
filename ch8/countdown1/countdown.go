f9ab6eb3ca2cd26b06e535e645477e04bd202fac
arkmadj <ahmadjinadu@gmail.com>
2023-02-21 06:26:11 +0100

    added main func

--- ch8/countdown1/countdown.go
+++ ch8/countdown1/countdown.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Commencing countdown.")
+	tick := time.Tick(1 * time.Second)
+	for countdown := 10; countdown > 0; countdown-- {
+		fmt.Println(countdown)
+		<-tick
+	}
+	launch()
+}
 
 func launch() {
 	fmt.Println("Lift off!")
