57abd1b884f424031af1e0a8f8f47aa7a6952a58
arkmadj <ahmadjinadu@gmail.com>
2023-02-21 06:32:55 +0100

    added abort

--- ch8/countdown2/countdown.go
+++ ch8/countdown2/countdown.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	abort := make(chan struct{})
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		abort <- struct{}{}
+	}()
+}
 
 func launch() {
 	fmt.Println("Lift off!")
