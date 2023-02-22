3cb6bf3d85c50a4000537ba6d42d1125ee08a5ac
arkmadj <ahmadjinadu@gmail.com>
2023-02-22 06:14:28 +0100

    add walk dir func

--- ch8/du1/main.go
+++ ch8/du1/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 )
 
 func dirents(dir string) []os.FileInfo {
@@ -13,4 +14,15 @@ func dirents(dir string) []os.FileInfo {
 		return nil
 	}
 	return entries
+}
+
+func walkDir(dir string, fileSizes chan<- int64) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subdir := filepath.Join(dir, entry.Name())
+			walkDir(subdir, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
 }
