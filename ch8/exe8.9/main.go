1431746d2eca8a937aa27fed172b63d5b64dbb70
arkmadj <ahmadjinadu@gmail.com>
2023-02-22 07:53:42 +0100

    added main func

--- ch8/exe8.9/main.go
+++ ch8/exe8.9/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
 )
 
 type SizeResponse struct {
@@ -49,4 +50,46 @@ func printDiskUsage(roots []string, nfiles, nbytes []int64) {
 	for i, r := range roots {
 		fmt.Printf("%10d files %.3f GB under %s\n", nfiles[i], float64(nbytes[i])/1e9, r)
 	}
+}
+
+func main() {
+	flag.Parse()
+
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	sizeResponses := make(chan SizeResponse)
+	var n sync.WaitGroup
+	for i, root := range roots {
+		n.Add(1)
+		go walkDir(root, &n, i, sizeResponses)
+	}
+	go func() {
+		n.Wait()
+		close(sizeResponses)
+	}()
+
+	var tick <-chan time.Time
+	if *vFlag {
+		tick = time.Tick(500 * time.Millisecond)
+	}
+	nfiles := make([]int64, len(roots))
+	nbytes := make([]int64, len(roots))
+
+loop:
+	for {
+		select {
+		case sr, ok := <-sizeResponses:
+			if !ok {
+				break loop
+			}
+			nfiles[sr.root]++
+			nbytes[sr.root] += sr.size
+		case <-tick:
+			printDiskUsage(roots, nfiles, nbytes)
+		}
+	}
+	printDiskUsage(roots, nfiles, nbytes)
 }
