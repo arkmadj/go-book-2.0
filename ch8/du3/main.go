f9148598d0337d364af685ed4b60d3d2cb8598d1
arkmadj <ahmadjinadu@gmail.com>
2023-02-22 06:52:44 +0100

    added dirents func

--- ch8/du3/main.go
+++ ch8/du3/main.go
@@ -1,3 +1,22 @@
 package main
 
-var sema = make(chan chan struct{}, 20)
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+var sema = make(chan struct{}, 20)
+
+func dirents(dir string) []os.FileInfo {
+	sema <- struct{}{}
+	defer func() {
+		<-sema
+	}()
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+		return nil
+	}
+	return entries
+}
