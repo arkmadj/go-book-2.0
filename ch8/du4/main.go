0e760a12ae2536d7a99d4ce534fd13498fc1d316
arkmadj <ahmadjinadu@gmail.com>
2023-02-22 07:10:06 +0100

    added cancelled func

--- ch8/du4/main.go
+++ ch8/du4/main.go
@@ -4,6 +4,15 @@ import "os"
 
 var done = make(chan struct{})
 
+func cancelled() bool {
+	select {
+	case <-done:
+		return true
+	default:
+		return false
+	}
+}
+
 var sema = make(chan struct{}, 20)
 
 func dirents(dir string) []os.FileInfo {
