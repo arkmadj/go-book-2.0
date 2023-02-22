51396c82c2feac47e5a9e92bf26d84e007df1b44
arkmadj <ahmadjinadu@gmail.com>
2023-02-22 07:30:16 +0100

    added main func

--- ch8/du4/main.go
+++ ch8/du4/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
 )
 
 var done = make(chan struct{})
@@ -60,4 +61,49 @@ func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
 
 func printDiskUsage(nfiles, nbytes int64) {
 	fmt.Printf("%d files %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
+
+func main() {
+	roots := os.Args[1:]
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	go func() {
+		os.Stdin.Read(make([]byte, 1))
+		close(done)
+	}()
+
+	fileSizes := make(chan int64)
+	var n sync.WaitGroup
+	for _, root := range roots {
+		n.Add(1)
+		go walkDir(root, &n, fileSizes)
+	}
+	go func() {
+		n.Wait()
+		close(fileSizes)
+	}()
+
+	tick := time.Tick(500 * time.Millisecond)
+	var nfiles, nbytes int64
+loop:
+	for {
+		select {
+		case <-done:
+			for range fileSizes {
+
+			}
+			return
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
+	printDiskUsage(nfiles, nbytes)
 }
