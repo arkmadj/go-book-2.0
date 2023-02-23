78f1308a234542a1f23500ecbeb40657f4d435f4
arkmadj <ahmadjinadu@gmail.com>
2023-02-23 06:26:53 +0100

    added base URL var

--- ch8/exe8.10/mirror.go
+++ ch8/exe8.10/mirror.go
@@ -1,8 +1,12 @@
 package main
 
-import "sync"
+import (
+	"net/url"
+	"sync"
+)
 
 var tokens = make(chan struct{}, 20)
 var maxDepth int
 var seen = make(map[string]bool)
 var seenLock = sync.Mutex{}
+var base *url.URL
