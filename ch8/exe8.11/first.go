450ba1e32c9c80a744753995e8a9958c7156b53c
arkmadj <ahmadjinadu@gmail.com>
2023-02-23 07:21:13 +0100

    added request to URL

--- ch8/exe8.11/first.go
+++ ch8/exe8.11/first.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"sync"
+)
+
+func main() {
+	flag.Parse()
+	cancel := make(chan struct{})
+	responses := make(chan *http.Response)
+	wg := &sync.WaitGroup{}
+	for _, url := range flag.Args() {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			req, err := http.NewRequest("HEAD", url, nil)
+			if err != nil {
+				log.Printf("HEAD %s: %s", url, err)
+				return
+			}
+			req.Cancel = cancel
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Printf("HEAD %s: %s", url, err)
+				return
+			}
+			responses <- resp
+		}(url)
+	}
+}
