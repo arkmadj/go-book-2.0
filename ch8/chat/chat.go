ce7bf3700e8ad00129e224601efec201eb6d7a90
arkmadj <ahmadjinadu@gmail.com>
2023-02-24 06:56:16 +0100

    added broadcaster func

--- ch8/chat/chat.go
+++ ch8/chat/chat.go
@@ -7,3 +7,20 @@ var (
 	leaving  = make(chan client)
 	mesages  = make(chan string)
 )
+
+func broadcaster() {
+	clients := make(map[client]bool)
+	for {
+		select {
+		case msg := <-mesages:
+			for cli := range clients {
+				cli <- msg
+			}
+		case cli := <-entering:
+			clients[cli] = true
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
