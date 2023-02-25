62276a10b8a1103202ffffe70f17760d97a33e5b
arkmadj <ahmadjinadu@gmail.com>
2023-02-25 23:59:27 +0100

    fixed typo

--- ch8/exe8.12/chat.go
+++ ch8/exe8.12/chat.go
@@ -46,7 +46,7 @@ func handleConn(conn net.Conn) {
 	who := conn.RemoteAddr().String()
 	cli := client{ch, who}
 	ch <- "You're " + who
-	messages <- who + " hasss arrived"
+	messages <- who + " hasss arrsived"
 	entering <- cli
 
 	input := bufio.NewScanner(conn)
