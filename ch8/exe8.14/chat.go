5649b67ea25cf14fa8b9f9de17720bfaf4459cc4
arkmadj <ahmadjinadu@gmail.com>
2023-02-27 06:37:19 +0100

    added broadcaster func

--- ch8/exe8.14/chat.go
+++ ch8/exe8.14/chat.go
@@ -5,7 +5,7 @@ import "time"
 const timeout = 10 * time.Second
 
 type client struct {
-	Out  <-chan string
+	Out  chan<- string
 	Name string
 }
 
@@ -14,3 +14,27 @@ var (
 	leaving  = make(chan client)
 	messages = make(chan string)
 )
+
+func broadcaster() {
+	clients := make(map[client]bool)
+	for {
+		select {
+		case msg := <-messages:
+			for cli := range clients {
+				select {
+				case cli.Out <- msg:
+				default:
+				}
+			}
+		case cli := <-entering:
+			clients[cli] = true
+			cli.Out <- "Present:"
+			for c := range clients {
+				cli.Out <- c.Name
+			}
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli.Out)
+		}
+	}
+}
