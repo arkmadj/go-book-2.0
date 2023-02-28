7e3808bbf62e732187008d272b7992fce8df542f
zultan <ahmad.jinadu@b54.co>
2023-02-28 06:55:03 +0100

    added testbank func

--- ch9/exe9.1/bank_test.go
+++ ch9/exe9.1/bank_test.go
@@ -1,1 +1,31 @@
 package bank
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBank(t *testing.T) {
+	done := make(chan struct{})
+
+	go func() {
+		Deposits(200)
+		Withdraw(200)
+		fmt.Println("=", Balance())
+		done <- struct{}{}
+	}()
+
+	go func() {
+		Deposits(50)
+		Withdraw(50)
+		Deposits(100)
+		done <- struct{}{}
+	}()
+
+	<-done
+	<-done
+
+	if got, want := Balance(), 100; got != want {
+		t.Errorf("Balance = %d, want %d", got, want)
+	}
+}
