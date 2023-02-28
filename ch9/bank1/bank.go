8e85659e789f9cd6ff48b0ac03ecdb40c0818058
zultan <ahmad.jinadu@b54.co>
2023-02-28 06:35:15 +0100

    added teller func

--- ch9/bank1/bank.go
+++ ch9/bank1/bank.go
@@ -5,3 +5,14 @@ var balances = make(chan int)
 
 func Deposit(amount int) { deposits <- amount }
 func Balance() int       { return <-balances }
+
+func teller() {
+	var balance int
+	for {
+		select {
+		case amount := <-deposits:
+			balance += amount
+		case balances <- balance:
+		}
+	}
+}
