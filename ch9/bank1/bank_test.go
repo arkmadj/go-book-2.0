c1e76b727f64956a3d9f9b3bf529ed4387b626db
zultan <ahmad.jinadu@b54.co>
2023-02-28 06:40:49 +0100

    added test for bank

---
+++ ch9/bank1/bank_test.go
@@ -0,0 +1,30 @@
+package bank_test
+
+import (
+	"fmt"
+	"testing"
+
+	bank "github.com/ahmad/go-book-2.0/ch9/bank1"
+)
+
+func TestBank(t *testing.T) {
+	done := make(chan struct{})
+
+	go func() {
+		bank.Deposit(200)
+		fmt.Println("=", bank.Balance())
+		done <- struct{}{}
+	}()
+
+	go func() {
+		bank.Deposit(100)
+		done <- struct{}{}
+	}()
+
+	<-done
+	<-done
+
+	if got, want := bank.Balance(), 300; got != want {
+		t.Errorf("Balance = %d, want %d", got, want)
+	}
+}
