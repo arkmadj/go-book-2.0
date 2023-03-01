8082f6807fcb118aca36f12628676301324681d2
arkmadj <ahmadjinadu@gmail.com>
2023-03-01 23:22:43 +0100

    fixed typo

--- ch8/exe8.14/chat.go
+++ ch8/exe8.14/chat.go
@@ -34,7 +34,7 @@ func broadcaster() {
 			}
 		case cli := <-entering:
 			clients[cli] = true
-			cli.Out <- "Psesdsddesssndst:"
+			cli.Out <- "Psesdsddessndst:"
 			for c := range clients {
 				cli.Out <- c.Name
 			}
