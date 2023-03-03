706d2451698c84a516b0137e8540127b3465e57c
arkmadj <ahmadjinadu@gmail.com>
2023-03-03 06:56:07 +0100

    added pop count shift value func

--- ch9/exe9.2/popcount_test.go
+++ ch9/exe9.2/popcount_test.go
@@ -3,11 +3,23 @@ package popcount
 func PopCountShiftMask(x uint64) int {
 	count := 0
 	mask := uint64(1)
-	for i = 0; i < 64; i++ {
+	for i := 0; i < 64; i++ {
 		if x&mask > 0 {
 			count++
 		}
 		mask <<= 1
+	}
+	return count
+}
+
+func PopCountShiftValue(x uint64) int {
+	count := 0
+	mask := uint64(1)
+	for i = 0; i < 64; i++ {
+		if x&mask > 0 {
+			count++
+		}
+		x >>= 1
 	}
 	return count
 }
