5993632545d40100df655b364311132674ea8637
arkmadj <ahmadjinadu@gmail.com>
2023-03-05 18:07:47 +0100

    adde sequential func

--- ch9/memo5/memo_test.go
+++ ch9/memo5/memo_test.go
@@ -1,5 +1,16 @@
 package memo_test
 
-import "github.com/ahmad/go-book-2.0/ch9/memotest"
+import (
+	"testing"
+
+	memo "github.com/ahmad/go-book-2.0/ch9/memo5"
+	"github.com/ahmad/go-book-2.0/ch9/memotest"
+)
 
 var httpGetBody = memotest.HTTPGetBody
+
+func Test(t *testing.T) {
+	m := memo.New(httpGetBody)
+	defer m.Close()
+	memotest.Sequential(t, m)
+}
