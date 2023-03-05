7019a1473a2caff49453b2e70dacfc6b1eafac23
arkmadj <ahmadjinadu@gmail.com>
2023-03-05 17:56:05 +0100

    added New func

--- ch9/memo5/memo.go
+++ ch9/memo5/memo.go
@@ -16,3 +16,11 @@ type request struct {
 	key      string
 	response chan<- result
 }
+
+type Memo struct{ requests chan request }
+
+func New(f Func) *Memo {
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f)
+	return memo
+}
