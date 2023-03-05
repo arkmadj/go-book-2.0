7455d9fb88f263bc61b2ffd8f890fa79d2d529f6
arkmadj <ahmadjinadu@gmail.com>
2023-03-05 12:45:59 +0100

    added Get Func

--- ch9/memo3/memo.go
+++ ch9/memo3/memo.go
@@ -18,3 +18,16 @@ type result struct {
 func New(f Func) *Memo {
 	return &Memo{f: f, cache: make(map[string]result)}
 }
+
+func (memo *Memo) Get(key string) (value interface{}, err error) {
+	memo.mu.Lock()
+	res, ok := memo.cache[key]
+	memo.mu.Unlock()
+	if !ok {
+		res.value, res.err = memo.f(key)
+		memo.mu.Lock()
+		memo.cache[key] = res
+		memo.mu.Unlock()
+	}
+	return res.value, res.err
+}
