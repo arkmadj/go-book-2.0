0256e8f38d1b58ee0dc3e6f9375527fb562ede28
arkmadj <ahmadjinadu@gmail.com>
2023-03-05 11:49:29 +0100

    added incomingURL func

--- ch9/memotest/memotest.go
+++ ch9/memotest/memotest.go
@@ -15,3 +15,23 @@ func httpGetBody(url string) (interface{}, error) {
 }
 
 var HTTPGetBody = httpGetBody
+
+func incomingURLs() <-chan string {
+	ch := make(chan string)
+	go func() {
+		for _, url := range []string{
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"http://gopl.io",
+			"https://golang.org",
+			"https://godoc.org",
+			"https://play.golang.org",
+			"http://gopl.io",
+		} {
+			ch <- url
+		}
+		close(ch)
+	}()
+	return ch
+}
