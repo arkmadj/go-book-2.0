ae78fb1e9ce6e5cf992f191889f9147cdf6a4e3d
arkmadj <ahmadjinadu@gmail.com>
2023-03-05 12:52:48 +0100

    added TestConcurrent func

--- ch9/memo3/main_test.go
+++ ch9/memo3/main_test.go
@@ -13,3 +13,8 @@ func Test(t *testing.T) {
 	m := memo.New(httpGetBody)
 	memotest.Sequential(t, m)
 }
+
+func TestConcurrent(t *testing.T) {
+	m := memo.New(httpGetBody)
+	memotest.Concurrent(t, m)
+}
