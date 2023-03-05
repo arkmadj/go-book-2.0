3872b083ef09d53654b2d5756bca041e6ae6fe6d
arkmadj <ahmadjinadu@gmail.com>
2023-03-05 11:46:58 +0100

    added httpGetBody func

--- ch9/memotest/memotest.go
+++ ch9/memotest/memotest.go
@@ -1,1 +1,15 @@
 package memotest
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
