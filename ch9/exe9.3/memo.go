862c9013bbe326be3d870fb3fe0be27b4a06df10
arkmadj <ahmadjinadu@gmail.com>
2023-03-05 18:34:15 +0100

    added Get func

--- ch9/exe9.3/memo.go
+++ ch9/exe9.3/memo.go
@@ -1,5 +1,7 @@
 package memo
 
+import "fmt"
+
 type Func func(key string, done <-chan struct{}) (interface{}, error)
 
 type result struct {
@@ -26,4 +28,21 @@ func New(f Func) *Memo {
 	memo := &Memo{make(chan request), make(chan request)}
 	go memo.server(f)
 	return memo
+}
+
+func (memo *Memo) Get(key string, done <-chan struct{}) (interface{}, error) {
+	response := make(chan result)
+	req := request{key, done, response}
+	memo.requests <- req
+	fmt.Println("get: waiting for response")
+	res := <-response
+	fmt.Println("get: checking if cancelled")
+	select {
+	case <-done:
+		fmt.Println("get: queueing cancellation request")
+		memo.cancels <- req
+	default:
+	}
+	fmt.Println("get: return")
+	return res.value, res.err
 }
