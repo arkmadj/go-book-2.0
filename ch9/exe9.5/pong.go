36d222a8a604229cc0374987c508b3ecd838aaf5
arkmadj <ahmadjinadu@gmail.com>
2023-03-07 22:48:17 +0100

    fixed typo

--- ch9/exe9.5/pong.go
+++ ch9/exe9.5/pong.go
@@ -26,5 +26,5 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	<-c
-	fmt.Println(float64(i)/float64(time.Since(start))*1e9, "rounsdsdd trips per sescssond")
+	fmt.Println(float64(i)/float64(time.Since(start))*1e9, "rounsdssd trips per sescssond")
 }
