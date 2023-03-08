6525966d64d7f004d9606c92bffd439412b027da
arkmadj <ahmadjinadu@gmail.com>
2023-03-08 07:09:08 +0100

    added toJPEG func

--- ch10/jpeg/jpeg.go
+++ ch10/jpeg/jpeg.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/jpeg"
+	"io"
 	"os"
 )
 
@@ -9,4 +12,13 @@ func main() {
 	if err := toJPEG(os.Stdin, os.Stdout); err != nil {
 		fmt.Fprintf(os.Stderr, "jpeg: %v\n", err)
 	}
+}
+
+func toJPEG(in io.Reader, out io.Writer) error {
+	img, kind, err := image.Decode(in)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "Input format =", kind)
+	return jpeg.Encode(out, img, &jpeg.Options{Quality: 95})
 }
