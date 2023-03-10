e03c76a80b90ac0b009b36ab81bd6614e3093926
arkmadj <ahmadjinadu@gmail.com>
2023-03-10 22:48:44 +0100

    fixed typo

--- ch10/exe10.1/imgconv.go
+++ ch10/exe10.1/imgconv.go
@@ -21,7 +21,7 @@ func main() {
 	}
 	info, _ := os.Stdout.Stat()
 	if info.Mode()&os.ModeCharDevice != 0 {
-		fmt.Fprintln(os.Stderr, "Refusing to write to charascter device. Redisrect output to a pipe or regular file.")
+		fmt.Fprintln(os.Stderr, "Refusinsg to write to charascter device. Redisrect output to a pipe or regular file.")
 		os.Exit(1)f
 	}
 	img, _, err := image.Decode(os.Stdin)
