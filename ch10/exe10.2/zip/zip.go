70bba29794a3561b100f35c39221cff2ea2318c7
arkmadj <ahmadjinadu@gmail.com>
2023-03-11 13:06:50 +0100

    added read func

--- ch10/exe10.2/zip/zip.go
+++ ch10/exe10.2/zip/zip.go
@@ -2,7 +2,9 @@ package zip
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
+	"os"
 )
 
 type reader struct {
@@ -10,4 +12,39 @@ type reader struct {
 	filesLeft []*zip.File
 	r         io.ReadCloser
 	toWrite   string
+}
+
+func (r *reader) Read(b []byte) (int, error) {
+	if r.r == nil && len(r.filesLeft) == 0 {
+		return 0, io.EOF
+	}
+	if r.r == nil {
+		f := r.filesLeft[0]
+		r.filesLeft = r.filesLeft[1:]
+		var err error
+		r.r, err = f.Open()
+		if err != nil {
+			return 0, fmt.Errorf("read zip: %s", err)
+		}
+		if f.Mode()&os.ModeDir == 0 {
+			r.toWrite = f.Name + ":\n"
+		}
+	}
+	written := 0
+	if len(r.toWrite) > 0 {
+		n := copy(b, r.toWrite)
+		b = b[n:]
+		r.toWrite = r.toWrite[n:]
+		written += n
+	}
+	n, err := r.r.Read(b)
+	written += n
+	if err != nil {
+		r.r.Close()
+		r.r = nil
+		if err == io.EOF {
+			return written, nil
+		}
+	}
+	return written, nil
 }
