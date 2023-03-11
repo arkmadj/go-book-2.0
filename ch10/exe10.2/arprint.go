812e849edd954ef7f4297ddeb1ea9cc1e7c8aea9
arkmadj <ahmadjinadu@gmail.com>
2023-03-11 12:41:20 +0100

    added NewReader type

--- ch10/exe10.2/arprint.go
+++ ch10/exe10.2/arprint.go
@@ -1,7 +1,14 @@
 package aprint
 
+import (
+	"io"
+	"os"
+)
+
 type format struct {
 	name, magic string
 	magicOffset int
 	reader      NewReader
 }
+
+type NewReader func(*os.File) (io.Reader, error)
