096eb83e34f27a6cee75efdd29de4262064e05ce
arkmadj <ahmadjinadu@gmail.com>
2023-03-11 12:43:21 +0100

    added register format func

--- ch10/exe10.2/arprint.go
+++ ch10/exe10.2/arprint.go
@@ -14,3 +14,7 @@ type format struct {
 type NewReader func(*os.File) (io.Reader, error)
 
 var formats []format
+
+func RegisterFormat(name, magic string, magicOffset int, f NewReader) {
+	formats = append(formats, format{name, magic, magicOffset, f})
+}
