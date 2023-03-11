309952d168e2c7fe6e1bd4a16921b93881ad713d
arkmadj <ahmadjinadu@gmail.com>
2023-03-11 12:55:58 +0100

    added read func

--- ch10/exe10.2/tar/tar.go
+++ ch10/exe10.2/tar/tar.go
@@ -2,6 +2,7 @@ package tar
 
 import (
 	"archive/tar"
+	"io"
 	"os"
 )
 
@@ -14,6 +15,29 @@ type reader struct {
 func (r *reader) Read(b []byte) (written int, err error) {
 	for len(b) > 0 {
 		if len(r.toWrite) > 0 {
+			n := copy(b, r.toWrite)
+			written += n
+			r.toWrite = r.toWrite[n:]
+			b = b[n:]
+		}
+		n, err := r.tarReader.Read(b)
+		written += n
+		b = b[n:]
+		switch err {
+		case io.EOF:
+			h, err := r.tarReader.Next()
+			if err != nil {
+				return written, err
+			}
+			if h.Typeflag == tar.TypeDir {
+				continue
+			}
+			r.toWrite = h.Name + ":\n"
+		case nil:
+			continue
+		default:
+			return written, err
 		}
 	}
+	return written, nil
 }
