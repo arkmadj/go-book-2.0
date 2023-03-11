fea2f877690e6f1870b9d45ca0b668b7ce0249b8
arkmadj <ahmadjinadu@gmail.com>
2023-03-11 19:05:51 +0100

    added main example func

--- ch10/exe10.2/cmd/list/list.go
+++ ch10/exe10.2/cmd/list/list.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"archive/tar"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func mainExample() {
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	tr := tar.NewReader(f)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("Contents of %s:\n", hdr.Name)
+		if _, err := io.Copy(os.Stdout, tr); err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println()
+	}
+}
