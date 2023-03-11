89790ec4f95b26ca7ca6c2e2b45ed04008008f27
arkmadj <ahmadjinadu@gmail.com>
2023-03-11 12:48:53 +0100

    added open file function

--- ch10/exe10.2/arprint.go
+++ ch10/exe10.2/arprint.go
@@ -1,6 +1,8 @@
 package aprint
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"os"
 )
@@ -17,4 +19,27 @@ var formats []format
 
 func RegisterFormat(name, magic string, magicOffset int, f NewReader) {
 	formats = append(formats, format{name, magic, magicOffset, f})
+}
+
+func Open(file *os.File) (io.Reader, error) {
+	var found *format
+	r := bufio.NewReader(file)
+	for _, f := range formats {
+		p, err := r.Peek(f.magicOffset + len(f.magic))
+		if err != nil {
+			continue
+		}
+		if string(p[f.magicOffset:]) == f.magic {
+			found = &f
+			break
+		}
+	}
+	if found == nil {
+		return nil, fmt.Errorf("open archive: can't determine format")
+	}
+	_, err := file.Seek(0, os.SEEK_SET)
+	if err != nil {
+		return nil, fmt.Errorf("open archive: %s", err)
+	}
+	return found.reader(file)
 }
