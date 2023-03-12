3d1c0be7e5c34706a6352a0e66ba9e21fdb96917
arkmadj <ahmadjinadu@gmail.com>
2023-03-12 17:39:29 +0100

    added packages func

--- ch10/exe10.4/ancestor.go
+++ ch10/exe10.4/ancestor.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func logCommandError(context string, err error) {
@@ -14,4 +15,14 @@ func logCommandError(context string, err error) {
 	log.Printf("%s: %s", context, err)
 	os.Stderr.Write(ee.Stderr)
 	os.Exit(1)
+}
+
+func packages(patterns []string) []string {
+	args := []string{"list", "-f{{.ImportPath}}"}
+	args = append(args, patterns...)
+	out, err := exec.Command("go", args...).Output()
+	if err != nil {
+		logCommandError("resolve packages", err)
+	}
+	return strings.Fields(string(out))
 }
