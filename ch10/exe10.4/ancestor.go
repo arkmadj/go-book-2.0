28df2236523b0ee4b74a5c97a533d8b30b056943
arkmadj <ahmadjinadu@gmail.com>
2023-03-12 17:45:46 +0100

    added ancestors func

--- ch10/exe10.4/ancestor.go
+++ ch10/exe10.4/ancestor.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"log"
 	"os"
 	"os/exec"
@@ -25,4 +27,31 @@ func packages(patterns []string) []string {
 		logCommandError("resolve packages", err)
 	}
 	return strings.Fields(string(out))
+}
+
+func ancestors(packageNames []string) []string {
+	targets := make(map[string]bool)
+	for _, pkg := range packageNames {
+		targets[pkg] = true
+	}
+
+	args := []string{"list", `-f={{.ImportPath}} {{join .Deps " "}}`, "..."}
+	out, err := exec.Command("go", args...).Output()
+	if err != nil {
+		logCommandError("find ancestors", err)
+	}
+	var pkgs []string
+	s := bufio.NewScanner(bytes.NewReader(out))
+	for s.Scan() {
+		fields := strings.Fields(s.Text())
+		pkg := fields[0]
+		deps := fields[1:]
+		for _, dep := range deps {
+			if targets[dep] {
+				pkgs = append(pkgs, pkg)
+				break
+			}
+		}
+	}
+	return pkgs
 }
