1b282150f122819cbe912007680be1473cad2603
arkmadj <ahmadjinadu@gmail.com>
2023-03-12 17:34:05 +0100

    added logCommandError

--- ch10/exe10.4/ancestor.go
+++ ch10/exe10.4/ancestor.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+func logCommandError(context string, err error) {
+	ee, ok := err.(*exec.ExitError)
+	if !ok {
+		log.Fatalf("%s: %s", context, err)
+	}
+	log.Printf("%s: %s", context, err)
+	os.Stderr.Write(ee.Stderr)
+	os.Exit(1)
+}
