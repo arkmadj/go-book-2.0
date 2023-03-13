63eca304ea1cb329b44f34bd1a5cca603fbe7259
arkmadj <ahmadjinadu@gmail.com>
2023-03-13 06:04:50 +0100

    added test palindrome func

--- ch11/word1/word_test.go
+++ ch11/word1/word_test.go
@@ -1,1 +1,12 @@
 package word
+
+import "testing"
+
+func TestPalindrome(t *testing.T) {
+	if !IsPalindrome("detartrated") {
+		t.Error(`IsPalindrome("detartrated) = false`)
+	}
+	if !IsPalindrome("kayak") {
+		t.Error(`IsPalindrome("kayak) = false`)
+	}
+}
