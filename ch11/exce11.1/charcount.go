ec9d62dc3b51238f0e2bc6a66d4435d4947f49db
arkmadj <ahmadjinadu@gmail.com>
2023-03-13 06:37:53 +0100

    added main func

--- ch11/exce11.1/charcount.go
+++ ch11/exce11.1/charcount.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"log"
+	"os"
 	"unicode"
 )
 
@@ -33,5 +35,27 @@ func charCount(r io.Reader) (runes map[rune]int, props map[string]int, sizes map
 		}
 		runes[r]++
 		sizes[n]++
+	}
+}
+
+func main() {
+	runes, props, sizes, invalid := charCount(os.Stdin)
+	fmt.Println(props)
+	fmt.Printf("rune\tcount\n")
+	for c, n := range runes {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range sizes {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	fmt.Printf("\n%-30s count\n", "category")
+	for cat, n := range props {
+		fmt.Printf("%-30s %d\n", cat, n)
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
 	}
 }
