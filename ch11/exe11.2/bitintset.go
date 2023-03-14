4f6994ff8d85dce6181823b91e830046a1bed671
arkmadj <ahmadjinadu@gmail.com>
2023-03-14 23:08:47 +0100

    added Add func

--- ch11/exe11.2/bitintset.go
+++ ch11/exe11.2/bitintset.go
@@ -8,3 +8,11 @@ func (s *BitIntSet) Has(x int) bool {
 	word, bit := x/64, uint(x%64)
 	return word < len(s.words) && s.words[word]&(1<<bit) != 0
 }
+
+func (s *BitIntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
