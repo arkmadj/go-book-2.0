908abe2cbf444889f838945e2c8acace05b89822
arkmadj <ahmadjinadu@gmail.com>
2023-03-14 22:47:44 +0100

    added has func

--- ch11/exe11.2/bitintset.go
+++ ch11/exe11.2/bitintset.go
@@ -3,3 +3,8 @@ package main
 type BitIntSet struct {
 	words []uint64
 }
+
+func (s *BitIntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
