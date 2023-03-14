80db65e13b0f44283cb99cebdedb6b9453087f07
arkmadj <ahmadjinadu@gmail.com>
2023-03-14 23:28:08 +0100

    added test zero inititally

--- ch11/exe11.2/intset_test.go
+++ ch11/exe11.2/intset_test.go
@@ -1,5 +1,15 @@
 package main
 
+import "testing"
+
 func newIntSets() []IntSet {
 	return []IntSet{&BitIntSet{}, NewMapIntSet()}
+}
+
+func TestLenZeroInitially(t *testing.T) {
+	for _, s := range newIntSets() {
+		if s.Len() != 0 {
+			t.Errorf("%T.len(): git %d, want 0", s, s.Len())
+		}
+	}
 }
