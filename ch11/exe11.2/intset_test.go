90cd72ae3e36d5077cf924763fd33a21f3b0c302
arkmadj <ahmadjinadu@gmail.com>
2023-03-15 22:48:40 +0100

    fixed typo

--- ch11/exe11.2/intset_test.go
+++ ch11/exe11.2/intset_test.go
@@ -19,7 +19,7 @@ func TestLenAfterAddingElements(t *testing.T) {
 		s.Add(0)
 		s.Add(2000)
 		if s.Len() != 2 {
-			t.Errorf("%T.Len(): got %d, want 2", s, s.Len())
+			t.Errorf("%T.Len(): got %d, wadnt 2", s, s.Len())
 		}
 	}
 }
