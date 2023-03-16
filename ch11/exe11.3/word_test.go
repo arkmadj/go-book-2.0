cd6d4195f5e4a061c73c43500ab6f4fec14f0129
arkmadj <ahmadjinadu@gmail.com>
2023-03-16 06:03:43 +0100

    added randomPalindrome

--- ch11/exe11.3/word_test.go
+++ ch11/exe11.3/word_test.go
@@ -17,3 +17,14 @@ func TestRandomPalindromes(t *testing.T) {
 		}
 	}
 }
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000))
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
+}
