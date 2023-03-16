0ecc6d11d2197754cad78cea743005996d6db83c
arkmadj <ahmadjinadu@gmail.com>
2023-03-16 05:50:44 +0100

    added test random palindrome

--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -3,6 +3,7 @@ package word
 import (
 	"math/rand"
 	"testing"
+	"time"
 )
 
 func TestIsPalindrome(t *testing.T) {
@@ -40,4 +41,17 @@ func randomPalindrome(rng *rand.Rand) string {
 		runes[n-1-i] = r
 	}
 	return string(runes)
+}
+
+func TestRandomPalindrome(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
 }
