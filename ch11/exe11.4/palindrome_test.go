890a80059615db05a2934e115dc4c7b8be6e3a8b
arkmadj <ahmadjinadu@gmail.com>
2023-03-16 06:35:15 +0100

    added TestRandomPalindromes func

--- ch11/exe11.4/palindrome_test.go
+++ ch11/exe11.4/palindrome_test.go
@@ -1,1 +1,19 @@
 package word
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
