5a4f7ac69ea8ab1a5f8785c9a853e8ce95a572d8
arkmadj <ahmadjinadu@gmail.com>
2023-03-16 05:46:13 +0100

    added randomPalindrome

--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -1,6 +1,9 @@
 package word
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func TestIsPalindrome(t *testing.T) {
 	var tests = []struct {
@@ -26,4 +29,15 @@ func TestIsPalindrome(t *testing.T) {
 			t.Errorf("IsPalindrome(%q) = %v", test.input, got)
 		}
 	}
+}
+
+func randomPalindrome(rng *rand.Rand) string {
+	n := rng.Intn(25)
+	runes := make([]rune, n)
+	for i := 0; i < (n+1)/2; i++ {
+		r := rune(rng.Intn(0x1000))
+		runes[i] = r
+		runes[n-1-i] = r
+	}
+	return string(runes)
 }
