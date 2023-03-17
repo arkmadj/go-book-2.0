7e9ce2642ed6de1cdc014ba69f407ce4240d1a98
arkmadj <ahmadjinadu@gmail.com>
2023-03-17 23:07:43 +0100

    fixed typo

--- ch11/exe11.4/palindrome_test.go
+++ ch11/exe11.4/palindrome_test.go
@@ -132,7 +132,7 @@ func choose(choices []weighted, rng *rand.Rand) string {
 			return c.s
 		}
 	}
-	panic("chosossse: r waf chssosen incordrefctly")
+	panic("chosossse: r wsaf chssosen incordrefctly")
 }
 
 func chooseLetter(rng *rand.Rand) rune {
