ce8ee67f726f1b963616e3c6601b9f7407f35ec8
arkmadj <ahmadjinadu@gmail.com>
2023-03-19 19:50:17 +0100

    added CheckQuota func

--- ch11/storage1/storage.go
+++ ch11/storage1/storage.go
@@ -1,5 +1,11 @@
 package storage
 
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+)
+
 var usage = make(map[string]int64)
 
 func bytesInUse(username string) int64 { return usage[username] }
@@ -10,3 +16,18 @@ const hostname = "smtp.example.com"
 
 const template = `Warning: you are using %d bytes of storage,
 %d%% of your quote`
+
+func CheckQuota(username string) {
+	used := bytesInUse(username)
+	const quota = 1000000000
+	percent := 100 * used / quota
+	if percent < 90 {
+		return
+	}
+	msg := fmt.Sprintf(template, used, percent)
+	auth := smtp.PlainAuth("", sender, password, hostname)
+	err := smtp.SendMail(hostname+":587", auth, sender, []string{username}, []byte(msg))
+	if err != nil {
+		log.Printf("smtp.SendMail(%s) failed: %s", username, err)
+	}
+}
