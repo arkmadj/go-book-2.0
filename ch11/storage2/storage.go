2fb0005127750d3c31e4352e8c06c002cd154735
arkmadj <ahmadjinadu@gmail.com>
2023-03-19 20:01:39 +0100

    added template string

--- ch11/storage2/storage.go
+++ ch11/storage2/storage.go
@@ -14,6 +14,8 @@ const sender = "notifications@example.com"
 const password = "correcthorsebatterystaple"
 const hostname = "smtp.example.com"
 
+const template = `Warning: you are using %d bytes of storage %d%% of your quota`
+
 var notifyUser = func(username, msg string) {
 	auth := smtp.PlainAuth("", sender, password, hostname)
 	err := smtp.SendMail(hostname+":587", auth, sender, []string{username}, []byte(msg))
