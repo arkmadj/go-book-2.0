c1ccf7da129223952c61b8a339cf15c15dc8d257
arkmadj <ahmadjinadu@gmail.com>
2023-03-19 19:55:15 +0100

    added sender, password and host name vars

--- ch11/storage2/storage.go
+++ ch11/storage2/storage.go
@@ -3,3 +3,7 @@ package storage
 var usage = make(map[string]int64)
 
 func bytesInUse(username string) int64 { return usage[username] }
+
+const sender = "notifications@example.com"
+const password = "correcthorsebatterystaple"
+const hostname = "smtp.example.com"
