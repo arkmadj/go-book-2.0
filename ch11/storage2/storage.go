1910134997d3142fb7cce7e29d6bc50db561a3a4
arkmadj <ahmadjinadu@gmail.com>
2023-03-19 19:58:40 +0100

    added notifyUser  var

--- ch11/storage2/storage.go
+++ ch11/storage2/storage.go
@@ -1,5 +1,10 @@
 package storage
 
+import (
+	"log"
+	"net/smtp"
+)
+
 var usage = make(map[string]int64)
 
 func bytesInUse(username string) int64 { return usage[username] }
@@ -7,3 +12,11 @@ func bytesInUse(username string) int64 { return usage[username] }
 const sender = "notifications@example.com"
 const password = "correcthorsebatterystaple"
 const hostname = "smtp.example.com"
+
+var notifyUser = func(username, msg string) {
+	auth := smtp.PlainAuth("", sender, password, hostname)
+	err := smtp.SendMail(hostname+":587", auth, sender, []string{username}, []byte(msg))
+	if err != nil {
+		log.Printf("smtp.SendMail(%s) failed: %s", username, err)
+	}
+}
