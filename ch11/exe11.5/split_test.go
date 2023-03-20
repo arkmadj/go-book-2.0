9b152295a315ae20de3c124dfc78f0f429a0fac4
arkmadj <ahmadjinadu@gmail.com>
2023-03-20 06:41:05 +0100

    removed failing test

--- ch11/exe11.5/split_test.go
+++ ch11/exe11.5/split_test.go
@@ -12,7 +12,7 @@ func TestSplit(t *testing.T) {
 	}{
 		{"a:b:c", ":", 3},
 		{"1,2,3,4,5", ",", 5},
-		{"x y z", " ", 4},
+		// {"x y z", " ", 4},
 	}
 	for _, test := range tests {
 		words := strings.Split(test.s, test.sep)
