fb2388d8d99d20dcee61f628769ce349b51fa7fe
arkmadj <ahmadjinadu@gmail.com>
2023-03-20 07:24:26 +0100

    added benchmark test

--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -55,3 +55,9 @@ func TestRandomPalindrome(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkIsPalindrome(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IsPalindrome("A man, a plan, a canal: Panama")
+	}
+}
