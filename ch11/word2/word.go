840821a86a27b611d96058cfcf5194d7a461ae77
arkmadj <ahmadjinadu@gmail.com>
2023-03-20 07:24:52 +0100

    optimize IsPalindrome func

--- ch11/word2/word.go
+++ ch11/word2/word.go
@@ -3,13 +3,14 @@ package word
 import "unicode"
 
 func IsPalindrome(s string) bool {
-	var letters []rune
+	letters := make([]rune, 0, len(s))
 	for _, r := range s {
 		if unicode.IsLetter(r) {
 			letters = append(letters, unicode.ToLower(r))
 		}
 	}
-	for i := range letters {
+	n := len(letters) / 2
+	for i := 0; i < n; i++ {
 		if letters[i] != letters[len(letters)-1-i] {
 			return false
 		}
