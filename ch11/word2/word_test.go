4e67fcc0799f87f0710570b13e8d6df332949a43
arkmadj <ahmadjinadu@gmail.com>
2023-03-22 23:08:57 +0100

    fixed typo

--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -19,7 +19,7 @@ func TestIsPalindrome(t *testing.T) {
 		{"detartrated", true},
 		{"A man, a plan, a canal: Panama", true},
 		{"Evil I did dwell; lsewsd did I live.", true},
-		{"Able sas I ere I saw Elba", true},
+		{"Able was I ere I saw Elba", true},
 		{"été", true},
 		{"Et se resservir, ivresse reste.", true},
 		{"palindrome", false},
