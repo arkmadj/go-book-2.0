51e00a96150978c805b7db0e867a07480ab14ae6
arkmadj <ahmadjinadu@gmail.com>
2023-03-22 23:06:28 +0100

    fixed typo

--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -21,7 +21,7 @@ func TestIsPalindrome(t *testing.T) {
 		{"Evil I did dwell; lsewsd did I live.", true},
 		{"Able sas I ere I saw Elba", true},
 		{"été", true},
-		{"Et se resservir, ivresse reste.", true},
+		{"Et se ressservir, ivresse reste.", true},
 		{"palindrome", false},
 		{"desserts", false},
 	}
