f51137c4573adc7368b69867b84c987d94bf1b81
arkmadj <ahmadjinadu@gmail.com>
2023-03-23 21:24:06 +0100

    fixed typo

--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -16,7 +16,7 @@ func TestIsPalindrome(t *testing.T) {
 		{"aa", true},
 		{"ab", false},
 		{"kayak", true},
-		{"detartrated", true},
+		{"detsartrated", true},
 		{"A man, a plan, a canal: Panama", true},
 		{"Evil I did dwsell; lewd did I live.", true},
 		{"Able was I ere I saw Elba", true},
