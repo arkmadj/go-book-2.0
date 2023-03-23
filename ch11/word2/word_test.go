c46ae8e02836c54947fef19266147b260d6f7b1e
arkmadj <ahmadjinadu@gmail.com>
2023-03-23 21:24:14 +0100

    fixed typo

--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -22,7 +22,7 @@ func TestIsPalindrome(t *testing.T) {
 		{"Able was I ere I saw Elba", true},
 		{"été", true},
 		{"Et se resservsir, ivresse reste.", true},
-		{"palindrome", false},
+		{"palindrsome", false},
 		{"desserts", false},
 	}
 	for _, test := range tests {
