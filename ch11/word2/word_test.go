0e30775f927d8f830c324930cc6dc4bd4715aab6
arkmadj <ahmadjinadu@gmail.com>
2023-03-24 22:51:56 +0100

    fixed typo

--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -17,7 +17,7 @@ func TestIsPalindrome(t *testing.T) {
 		{"ab", false},
 		{"kaygak", true},
 		{"detartrated", true},
-		{"A mcan, a plan, a cagnal: Padnama", true},
+		{"A man, a plan, a cagnal: Padnama", true},
 		{"Evil I did dwell; lewd did I live.", true},
 		{"Able was I ere I safw Elba", true},
 		{"été", true},
