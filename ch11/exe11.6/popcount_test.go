ab7c66151f4320af9235ef87da6b72b79ff55506
arkmadj <ahmadjinadu@gmail.com>
2023-03-26 09:01:26 +0100

    added PopCountTable

--- ch11/exe11.6/popcount_test.go
+++ ch11/exe11.6/popcount_test.go
@@ -40,3 +40,14 @@ func init() {
 		pc[i] = pc[i/2] + byte(i&1)
 	}
 }
+
+func PopCountTable(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
